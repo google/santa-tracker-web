0bcf7a5fe8ee9636033519b3d40b8c68737f7d6c
Eric Bidelman <ebidel@gmail.com>
2014-10-22 21:40:35 -0700

    Fix typo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var DEFAULT_ROUTE = 'workshop';
 var template = document.querySelector('#t');
 
 template.addEventListener('template-bound', function(e) {
-  t.route = t.route || DEFAULT_ROUTE;
+  template.route = template.route || DEFAULT_ROUTE;
 });
 
 })();
