b860c52e8f99d1d99abf731aec0065c056cc4fc0
devnook <devnook@gmail.com>
2014-12-03 11:56:20 +0000

    Reverting traditions

--- scenes/traditions/js/traditions.js
+++ scenes/traditions/js/traditions.js
@@ -55,11 +55,6 @@ Traditions.prototype.setup = function() {
   };
 
   /**
-   * @private
-   */
-  this.santaMap_ = new SantaMap(this.el_.querySelector('#traditions-map'));
-
-  /**
    * @private {google.maps.LatLngBounds}
    */
   this.markerBounds_ = new google.maps.LatLngBounds();
@@ -76,7 +71,63 @@ Traditions.prototype.setup = function() {
 Traditions.LANGUAGES_ = ['en', 'en-GB'];
 
 Traditions.prototype.onShow = function() {
-  this.map_ = this.santaMap_.createMap();
+  this.map_ = new google.maps.Map(this.el_.querySelector('#traditions-map'), {
+    center: new google.maps.LatLng(0, 0),
+    zoom: 1,
+    disableDefaultUI: true,
+    scrollwheel: false,
+    draggable: false,
+    disableDoubleClickZoom: true,
+    backgroundColor: Traditions.WATER_COLOR_,
+    // It's important that we have map styles -- this prevents a call to
+    // staticmap.
+    styles: [{
+      'stylers': [
+        { 'visibility': 'off' }
+      ]
+    },{
+      'featureType': 'water',
+      'stylers': [
+        { 'visibility': 'on' },
+        { 'color': Traditions.WATER_COLOR_ }
+      ]
+    },{
+      'featureType': 'landscape',
+      'stylers': [
+        { 'visibility': 'on' },
+        { 'lightness': 110 },
+        { 'color': '#dfd7c5' }
+      ]
+    },{
+      'featureType': 'administrative.country',
+      'elementType': 'geometry.stroke',
+      'stylers': [
+        { 'visibility': 'on' },
+        { 'invert_lightness': true },
+        { 'gamma': 3.75 },
+        { 'lightness': 70 },
+        { 'weight': 1 }
+      ]
+    },{
+      'featureType': 'water',
+      'elementType': 'labels',
+      'stylers': [
+        { 'weight': 0.8 },
+        { 'color': '#c5c6c4' },
+        { 'lightness': 5 }
+      ]
+    },{
+      'featureType': 'landscape.natural',
+      'elementType': 'labels',
+      'stylers': [
+        { 'visibility': 'on' },
+        { 'weight': 0.3 },
+        { 'invert_lightness': true },
+        { 'lightness': 40 }
+      ]
+    }]
+  });
+
   this.addCountryMarkers_();
 
   $(window).on('resize.traditions', this.handleResize_.bind(this));
@@ -220,7 +271,7 @@ Traditions.prototype.handleResize_ = function() {
       new google.maps.LatLng(country.info.geometry.viewport.northeast.lat,
                              country.info.geometry.viewport.northeast.lng)
     );
-    this.map_.fitBounds(SantaMap.padBounds_(viewport));
+    this.map_.fitBounds(this.padBounds_(viewport));
   }
 };
 
@@ -303,6 +354,18 @@ Traditions.prototype.show = function(id) {
   this.handleResize_();
 };
 
+/**
+ * Adds padding to the latlng bounds for each country.
+ * @return {!google.maps.LatLng}
+ */
+Traditions.prototype.padBounds_ = function(bounds) {
+  var width = bounds.toSpan().lng();
+  var newSW = new google.maps.LatLng(
+    bounds.getSouthWest().lat(),
+    bounds.getSouthWest().lng() - width * .3);
+
+  return bounds.extend(newSW);
+};
 
 /**
  * @return {jQuery}
