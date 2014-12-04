2473bd77c0774260d5d1dd5ac6da5d3204327f16
Ægir Giraldo Þorsteinsson <aegir@thorsteinsson.is>
2014-12-04 02:34:43 +0000

    Scope keyboard event handler to the scene element.

--- scenes/shared/js/startbutton.js
+++ scenes/shared/js/startbutton.js
@@ -1,24 +1,30 @@
 /**
  * Display a start button on top of an element, hiding and calling a callback
  * when it's clicked. Used to start games and user initiate webaudio.
+ * @param {HTMLElement} sceneElem The element with key events.
  * @param {HTMLElement} elem The container for the button.
  * @param {Function} callback The function to call when button is pressed.
  */
-function startButton(elem, callback) {
-  var button = $('<div class="start"><div class="start-button"></div></div>');
-  $(elem).append(button);
-  button.find('.start-button').on('mouseenter', function() {
+function startButton(sceneElem, elem, callback) {
+  sceneElem = $(sceneElem);
+  elem = $(elem);
+  this.callback = callback;
+
+  var buttonElem = $('<div class="start"><div class="start-button"></div></div>');
+  elem.append(buttonElem);
+
+  buttonElem.find('.start-button').on('mouseenter', function() {
     window.santaApp.fire('sound-trigger', 'generic_button_over');
   });
-  button.one('click', function() {
-    $(window).off('.startbutton');
+  buttonElem.one('click', function() {
+    sceneElem.off('.startbutton');
     window.santaApp.fire('sound-trigger', 'generic_button_click');
     callback();
-    button.remove();
+    buttonElem.remove();
   });
-  $(window).on('keydown.startbutton', function(event) {
+  sceneElem.on('keydown.startbutton', function(event) {
     if (event.keyCode === 13) {
-      button.trigger('click');
+      buttonElem.trigger('click');
     }
   });
 }
