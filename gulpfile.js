96a588334e9901cdcef10881ed7ae681fad31873
Brendan Kenny <bckenny@google.com>
2014-12-12 23:15:06 -0800

    Tag 63

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ var COMPILER_PATH = 'components/closure-compiler/compiler.jar';
 var COMPASS_FILES = '{scenes,sass,elements}/**/*.scss';
 var CLOSURE_FILES = 'scenes/*/js/**/*.js';
 
-var STATIC_VERSION = 62;
+var STATIC_VERSION = 63;
 var VERSION = argv.build || STATIC_VERSION;
 
 var STATIC_BASE_URL = argv.baseurl ? argv.baseurl : '';
