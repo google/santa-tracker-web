9fd897ffea29b32df1a0278122d62bdbfc59cd05
Sam Thorogood <sam.thorogood@gmail.com>
2015-02-16 12:02:21 +1100

    mercator js cleanup

--- scenes/mercator/js/utils.js
+++ scenes/mercator/js/utils.js
@@ -8,9 +8,9 @@ goog.provide('app.utils');
 app.utils = {
   /**
    * Convert a point to a latitude and longitude.
-   * @param {google.maps.Map} map The Google map.
-   * @param {google.maps.Point} point The point to convert.
-   * @return {google.maps.LatLng} The latitude and longitude.
+   * @param {!google.maps.Map} map The Google map.
+   * @param {!google.maps.Point} point The point to convert.
+   * @return {!google.maps.LatLng} The latitude and longitude.
    */
   pointToLatLng: function(map, point) {
     var scale = Math.pow(2, map.getZoom());
@@ -20,9 +20,9 @@ app.utils = {
 
   /**
    * Convert a latitude and longitude to a point.
-   * @param {google.maps.Map} map The Google map.
-   * @param {google.maps.LatLng} latLng The latitude and longitude to convert.
-   * @return {google.maps.Point} The point.
+   * @param {!google.maps.Map} map The Google map.
+   * @param {!google.maps.LatLng} latLng The latitude and longitude to convert.
+   * @return {!google.maps.Point} The point.
    */
   latLngToPoint: function(map, latLng) {
     var point = map.getProjection().fromLatLngToPoint(latLng);
