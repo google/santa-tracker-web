eea6c1ebb794b6f6fc42cef1fd3580a18ac9f825
Luke Mahe <lukem@lukem-macbookpro3.roam.corp.google.com>
2015-11-10 13:25:27 +1100

    comment fixes

--- gulpfile.js
+++ gulpfile.js
@@ -469,12 +469,12 @@ gulp.task('watch', function() {
 gulp.task('default', ['copy-assets']);
 
 gulp.task('serve', ['compass', 'compile-scenes'], function() {
-    browserSync.init({
-        server: "."
-    });
+  browserSync.init({
+	  server: '.'
+  });
 
-    gulp.watch("sass/*.scss", ['compass']).on("change", browserSync.reload);
-	  gulp.watch(COMPASS_FILES, ['compass']).on("change", browserSync.reload);
-	  gulp.watch(CLOSURE_FILES, ['compile-scenes']).on("change", browserSync.reload);
-		gulp.watch("scenes/**/*.html").on("change", browserSync.reload);	
+  gulp.watch('sass/*.scss', ['compass']).on('change', browserSync.reload);
+  gulp.watch(COMPASS_FILES, ['compass']).on('change', browserSync.reload);
+  gulp.watch(CLOSURE_FILES, ['compile-scenes']).on('change', browserSync.reload);
+	gulp.watch('scenes/**/*.html').on('change', browserSync.reload);	
 });
