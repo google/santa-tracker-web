fc1e3fc62d8ac3cb145f9d659c10c7eea7f5945d
Marco Barbosa <marco.barbosa84@gmail.com>
2015-11-30 10:03:25 +0100

    dropper: making the button the atual CTA

--- scenes/presentbounce/js/world/dropper.js
+++ scenes/presentbounce/js/world/dropper.js
@@ -39,17 +39,18 @@ goog.scope(function () {
     constructor(...args) {
       super(...args); // super(...arguments) doesn't work in Closure Compiler
       this.body_ = this.buildBody_();
+      this.$button_ = this.$el_.find('.js-dropper__button');
 
       this.onDropClick = this.onDropClick.bind(this);
       this.addEventListeners_();
     }
 
     addEventListeners_() {
-      this.$el_.on(app.InputEvent.START, this.onDropClick);
+      this.$button_.on(app.InputEvent.START, this.onDropClick);
     }
 
     removeEventListeners_() {
-      this.$el_.off(app.InputEvent.START, this.onDropClick);
+      this.$button_.off(app.InputEvent.START, this.onDropClick);
     }
 
     onDropClick() {
