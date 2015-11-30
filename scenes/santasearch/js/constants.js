bd247193d78663c2ac856f8a9757ba0f7a594489
Ægir Giraldo Þorsteinsson <aegir@thorsteinsson.is>
2015-11-30 15:04:10 -0600

    Fix devmode

--- scenes/santasearch/js/constants.js
+++ scenes/santasearch/js/constants.js
@@ -13,6 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+ 'use strict';
 
 goog.provide('Constants');
 goog.provide('app.Constants');
