875cc07a83b986f3bb74d1138f3875adce355301
Sam Thorogood <sam.thorogood@gmail.com>
2015-12-02 19:42:47 +1100

    fix snow on older Safaris and (very) old Chrome

--- gulpfile.js
+++ gulpfile.js
@@ -105,8 +105,7 @@ var SHARED_EXTERNS = [
   'components/web-animations-utils/externs*.js'
 ];
 
-// nb. chrome >=42 for Portal mode in 2015.
-var AUTOPREFIXER_BROWSERS = ['> 2%', 'ios_saf >= 8', 'ie >= 11', 'chrome >= 42'];
+var AUTOPREFIXER_BROWSERS = ['> 2%', 'ios_saf >= 8', 'ie >= 11'];
 
 var CLOSURE_WARNINGS = [
   // https://github.com/google/closure-compiler/wiki/Warnings
