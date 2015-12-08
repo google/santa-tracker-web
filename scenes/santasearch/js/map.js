2424f6b7b326244ecd8d65724bedd33920a2ddb7
Sam Thorogood <sam.thorogood@gmail.com>
2015-12-08 20:05:00 +1100

    match more url( params in SVG

--- scenes/santasearch/js/map.js
+++ scenes/santasearch/js/map.js
@@ -114,14 +114,17 @@ app.Map.prototype.loadMap_ = function(mapName, mapDimensions) {
     // Work around base href, which causes all inline IDs to refer to the base
     // href in production (which is served from maps.gstatic.com...). Refer
     // to the local pageUrl instead, since the clippath elements are inlined.
+    var re = /^url\((["']?)#/;  // match `url("#` with optional quote in group 1
+    var candidates = ['clipPath', 'stroke', 'fill'];
     var pageUrl = location.href.substr(0, location.href.length - location.hash.length);
     var all = svgMap.querySelectorAll('[style]');
     for (var i = 0, el; el = all[i]; ++i) {
       var s = el.style;
-      if (s.clipPath) {
-        // nb. assumes `url("#` or `url(#` ...
-        s.clipPath = s.clipPath.replace('#', `${pageUrl}#`);
-      }
+      candidates.forEach(function(c) {
+        if (s[c]) {
+          s[c] = s[c].replace(re, 'url($1' + pageUrl + '#');
+        }
+      });
     }
 
     // Add the new map into the dom
