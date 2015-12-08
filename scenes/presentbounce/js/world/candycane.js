074505fe81300ac5b388e44ebb116b747994f393
Marco Barbosa <marco.barbosa84@gmail.com>
2015-12-08 18:15:11 -0200

    candycane: only play sound if its a present hit

--- scenes/presentbounce/js/world/candycane.js
+++ scenes/presentbounce/js/world/candycane.js
@@ -52,8 +52,11 @@ goog.scope(function () {
      */
     onCollision(contact) {
       // check if it's a ball before playing 'boing' the sound
-      if (contact && contact.GetFixtureA().collisionID === PresentBall.COLLISION_ID ||
-          contact.GetFixtureA().collisionID === PresentSquare.COLLISION_ID
+      if (contact &&
+          contact.GetFixtureA().collisionID === PresentBall.COLLISION_ID   ||
+          contact.GetFixtureA().collisionID === PresentSquare.COLLISION_ID ||
+          contact.GetFixtureB().collisionID === PresentBall.COLLISION_ID   ||
+          contact.GetFixtureB().collisionID === PresentSquare.COLLISION_ID
       ) {
         window.santaApp.fire('sound-trigger', 'pb_wall');
       }
