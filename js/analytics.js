f6d6d9068e631f125900bb5a2178fdc52e589fd6
Sam Thorogood <samthor@users.noreply.github.com>
2016-11-28 10:09:06 +0000

    unify sharing in santa-app (#1917)
    * unify sharing in santa-app
    
    * remove log

--- js/analytics.js
+++ js/analytics.js
@@ -103,18 +103,6 @@ Analytics.prototype.trackEvent = function(category, action, opt_label, opt_value
 };
 
 /**
- * Tracks a social event
- *
- * @param {string} network
- * @param {string} action
- * @param {string} target
- * @export
- */
-Analytics.prototype.trackSocial = function(network, action, target) {
-  window.ga('send', 'social', network, action, target);
-};
-
-/**
  * Tracks that a game has started.
  * @param {string} gameId module id.
  * @export
