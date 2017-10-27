fdef732c83b0ae62ac87520d3da214c82b547ef0
Le <le@upperquad.com>
2017-10-27 15:23:11 -0700

    Make other drawing tools draw

--- scenes/clausedraws/js/tools.js
+++ scenes/clausedraws/js/tools.js
@@ -15,12 +15,14 @@
  */
 
 goog.provide('app.Tools');
+goog.require('app.Crayon');
 goog.require('app.Eraser');
 goog.require('app.LayerTool');
-goog.require('app.Stamp');
+goog.require('app.Marker');
 goog.require('app.Pen');
 goog.require('app.Slider');
 goog.require('app.SprinkleSpray');
+goog.require('app.Stamp');
 goog.require('app.Tool');
 goog.require('app.utils');
 
@@ -45,6 +47,8 @@ app.Tools = function(game, $elem) {
   this.categoryPickers.on('click.clausedraws', this.onCategoryClick_.bind(this));
 
   this.pen = new app.Pen($elem, 'pen');
+  this.crayon = new app.Crayon($elem, 'crayon');
+  this.marker = new app.Marker($elem, 'marker');
   this.sprinkles = new app.SprinkleSpray($elem, 'spray-sprinkles');
   this.snowbg = new app.LayerTool($elem, 'scene-snowbg', app.LayerTool.Layer.BACKGROUND, $elem.find('#snowbg')[0]);
   this.snowfg = new app.LayerTool($elem, 'scene-snowfg', app.LayerTool.Layer.FOREGROUND, $elem.find('#snowfg')[0]);
@@ -56,6 +60,8 @@ app.Tools = function(game, $elem) {
 
   this.tools = [
     this.pen,
+    this.crayon,
+    this.marker,
     this.sprinkles,
     this.snowbg,
     this.snowfg,
