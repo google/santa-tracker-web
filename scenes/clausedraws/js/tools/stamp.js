5d23c912461243dba72dc5856c2bb6cfa147bf02
Le <le@upperquad.com>
2017-10-30 16:52:09 -0700

    Bow stamp cursor

--- scenes/clausedraws/js/tools/stamp.js
+++ scenes/clausedraws/js/tools/stamp.js
@@ -34,6 +34,7 @@ app.Stamp = function($elem, name, stamp) {
   this.stamp = stamp;
   this.soundKey = 'selfie_spray_small';
   this.stamped = false;
+  this.sizeMultiplier = 1;
 };
 app.Stamp.prototype = Object.create(app.Tool.prototype);
 
@@ -43,23 +44,19 @@ app.Stamp.prototype = Object.create(app.Tool.prototype);
  * @param  {!HTMLCanvasElement} canvas The canvas to draw to
  * @param  {!app.Canvas.CoordsType} mouseCoords Mouse coords
  * @param  {!HTMLCanvasElement} prevCanvas  The previously saved canvas
- * @param  {!number} size  The current size setting
  */
-app.Stamp.prototype.draw = function(canvas, mouseCoords, prevCanvas, size) {
+app.Stamp.prototype.draw = function(canvas, mouseCoords, prevCanvas) {
   if (this.stamped) {
     return false;
   }
 
   var context = canvas.getContext('2d');
-  var sizeMultiplier = app.utils.map(size, app.Constants.STAMP_MIN,
-      app.Constants.STAMP_MAX);
-
-  var drawWidth = this.stamp.width / mouseCoords.scale * sizeMultiplier;
-  var drawHeight = this.stamp.height / mouseCoords.scale * sizeMultiplier;
+  var drawWidth = this.stamp.width * this.sizeMultiplier;
+  var drawHeight = this.stamp.height * this.sizeMultiplier;
   var drawX = mouseCoords.normX * canvas.width;
   var drawY = mouseCoords.normY * canvas.height;
-  var offsetX = this.mouseOffset.x / mouseCoords.scale;
-  var offsetY = this.mouseOffset.y / mouseCoords.scale;
+  var offsetX = drawWidth / 2;
+  var offsetY = drawHeight / 2;
   context.drawImage(this.stamp,
     drawX - offsetX, drawY - offsetY,
     drawWidth, drawHeight);
@@ -93,4 +90,22 @@ app.Stamp.prototype.stopMousedown = function() {
 app.Stamp.prototype.reset = function() {
   this.stamped = false;
 };
+
+
+/**
+ * Updates size indicator
+ * @param  {!number} size  The current size setting
+ */
+app.Stamp.prototype.updateSize = function(size) {
+  this.sizeMultiplier = app.utils.map(size, app.Constants.STAMP_MIN,
+      app.Constants.STAMP_MAX);
+  this.currentSize = this.calculateDrawSize(size);
+}
+
+
+app.Stamp.prototype.calculateDrawSize = function(size) {
+  return Math.max(this.stamp.width * this.sizeMultiplier,
+      this.stamp.height * this.sizeMultiplier);
+}
+
 
