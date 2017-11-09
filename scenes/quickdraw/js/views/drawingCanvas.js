f14c7e13ee81ddc3ff593f363911897eadc30dca
Louis <louis.amiot@hetic.net>
2017-11-08 16:30:22 -0800

    Add segments data to drawing Canvas

--- scenes/quickdraw/js/views/drawingCanvas.js
+++ scenes/quickdraw/js/views/drawingCanvas.js
@@ -19,6 +19,7 @@ goog.provide('app.view.DrawingCanvas');
 goog.require('app.EventEmitter');
 goog.require('Paper');
 
+
 app.view.DrawingCanvas = function(container) {
   app.EventEmitter.call(this);
 
@@ -31,6 +32,7 @@ app.view.DrawingCanvas = function(container) {
   var strokeWidth = 7;
   var strokeCap = 'round';
   this.previousPos = null;
+  this.paths = [];
 
   paper.install(app);
   paper.setup(this.canvas);
@@ -51,28 +53,74 @@ app.view.DrawingCanvas = function(container) {
         strokeWidth: strokeWidth
     });
 
+    if (this.paths.length == 0) {
+      this.startTime = new Date();
+    }
+
+    this.paths.push([[event.point.x],[event.point.y],[this.currentTimeMs()]]);
+
     paper.view.draw();
-  };
+  }.bind(this);
 
   tool.onMouseDrag = function(event) {
     path.add(event.point);
+
+    var arr = this.paths[this.paths.length - 1];
+    if (arr[0].length == 0
+      || Math.abs(arr[0][arr[0].length - 1] - event.point.x) > 4
+      || Math.abs(arr[1][arr[1].length - 1]- event.point.y) > 4) {
+      arr[0].push(event.point.x)
+      arr[1].push(event.point.y)
+      arr[2].push(this.currentTimeMs())
+    }
+
     path.smooth();
     paper.view.draw();
-  };
+    this.emit('DRAWING_UPDATED', this);
+  }.bind(this);
 
   tool.onMouseUp = function(event) {
     path.smooth();
     paper.view.draw();
-  };
+    this.emit('DRAWING_UPDATED', this);
 
-  //this.resizeCanvas();
+    this.paths[this.paths.length - 1][0].push(event.point.x);
+    this.paths[this.paths.length - 1][1].push(event.point.y);
+    this.paths[this.paths.length - 1][2].push(this.currentTimeMs());
+  }.bind(this);
 
-  // $(window).on('resize', function() {
-  //   this.resizeCanvas();
-  // }.bind(this));
+  this.resizeCanvas();
+
+  $(window).on('resize', function() {
+    this.resizeCanvas();
+  }.bind(this));
 };
 
+
 app.view.DrawingCanvas.prototype = Object.create(app.EventEmitter.prototype);
+
+
+app.view.DrawingCanvas.prototype.currentTimeMs = function() {
+  if(!this.startTime) {
+    return 0;
+  }
+  return (new Date()) - this.startTime;
+};
+
+
+app.view.DrawingCanvas.prototype.clear = function() {
+  project.activeLayer.removeChildren();
+  this.resizeCanvas();
+
+  delete this.startTime;
+  this.paths = []
+};
+
+
+app.view.DrawingCanvas.prototype.getSegments = function() {
+  return this.paths;
+};
+
 
 app.view.DrawingCanvas.prototype.resizeCanvas = function() {
   var pCenter = paper.view.center;
