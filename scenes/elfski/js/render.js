b9986aab9423443fb06fb8d5c6a9b4c2bd8061d1
Sam Thorogood <sam.thorogood@gmail.com>
2017-11-11 11:15:32 +1100

    refactoring renderable vs game

--- scenes/elfski/js/render.js
+++ scenes/elfski/js/render.js
@@ -16,358 +16,170 @@
 
 import * as webgl from './webgl.js';
 
-const spriteVertexShader = `
-uniform vec2 u_screenDims;
-
-// Center of the sprite in screen coordinates
-attribute vec2 centerPosition;
-
-// Offset of the sprite's origin.
-attribute float offsetY;
-
-// Layer to push this sprite into.
-attribute float layer;
-
-// Transform of the whole screen.
-uniform vec2 u_transform;
-
-// Rotation to draw sprite at
-attribute float rotation;
-
-// Per-sprite frame offset.
-attribute float spriteIndex;
-
-// Sprite size in screen coordinates
-attribute float spriteSize;
-
-// Offset of this vertex's corner from the center, in normalized
-// coordinates for the sprite. In other words:
-//   (-0.5, -0.5) => Upper left corner
-//   ( 0.5, -0.5) => Upper right corner
-//   (-0.5,  0.5) => Lower left corner
-//   ( 0.5,  0.5) => Lower right corner
-attribute vec2 cornerOffset;
-
-// Specified in normalized coordinates (0.0..1.0), where 1.0 = spriteSize.
-attribute vec2 spriteTextureSize;
-
-// Number of sprites per row of texture
-attribute float spritesPerRow;
-
-// Output to the fragment shader.
-varying vec2 v_texCoord;
-
-void main() {
-  float row = floor(spriteIndex / spritesPerRow);
-  float col = (spriteIndex - (row * spritesPerRow));
-
-  vec2 upperLeftTC = vec2(spriteTextureSize.x * col, spriteTextureSize.y * row);
-
-  // Get the texture coordinate of this vertex (cornerOffset is in [-0.5,0.5])
-  v_texCoord = upperLeftTC + spriteTextureSize * (cornerOffset + vec2(0.5, 0.5));
-
-  // Shift to center of screen, base of sprite.
-  // TODO: We could make the origin configurable.
-  vec2 halfDims = u_screenDims / 2.0;
-  vec2 updateCenter = vec2(centerPosition.x + halfDims.x,
-                           centerPosition.y + halfDims.y - spriteSize / 2.0 + offsetY);
-
-  // Rotate as appropriate
-  float s = sin(rotation);
-  float c = cos(rotation);
-  mat2 rotMat = mat2(c, -s, s, c);
-  vec2 scaledOffset = spriteSize * cornerOffset;
-  vec2 pos = updateCenter + rotMat * scaledOffset;
-
-  // depth goes from 0-1, where 0=(-screenDims.y) and 1=(2*screenDims.y)
-  float depthRange = u_screenDims.y * 3.0;
-  float depth = 1.0 - (updateCenter.y + u_screenDims.y + u_transform.y) / depthRange;
-  depth = depth - layer;
-
-  vec4 screenTransform = vec4(2.0 / u_screenDims.x, -2.0 / u_screenDims.y, -1.0, 1.0);
-  gl_Position = vec4((pos + u_transform) * screenTransform.xy + screenTransform.zw, depth, 1.0);
-}
-`;
-
-const spriteFragmentShader = `
-precision mediump float;
-
-uniform sampler2D u_texture;
-
-varying vec2 v_texCoord;
-
-void main() {
-  vec4 color = texture2D(u_texture, v_texCoord);
-
-  if (color.a == 0.0) {
-    // sanity discard if blend mode is bad
-    discard;
-  }
-
-  gl_FragColor = color;
-}
-`;
-
 /**
- * @type {!Array<!Array<number>>}
+ * Renderable manages memory and the allocation/free of verticies for a wrapped
+ * webgl.ShaderProgram.
  */
-const offsets = [
-  [-0.5, -0.5],
-  [-0.5,  0.5],
-  [ 0.5, -0.5],
-  [ 0.5, -0.5],
-  [-0.5,  0.5],
-  [ 0.5,  0.5],
-];
+export class Renderable {
+  /**
+   * @param {!webgl.ShaderProgram} program
+   */
+  constructor(program) {
+    this._gl = program.gl;
+    this._program = program;
 
-/**
- * @typedef {{
- *   at: {x: number, y: number},
- *   rotation: (undefined|number),
- *   offset: (undefined|number),
- *   layer: (undefined|number),
- *   spriteIndex: number
- * }}
- */
-var SpriteDef;
+    /** @private {!Float32Array} */
+    this._data = new Float32Array(0);
 
-const zeroTransform = Object.freeze({x: 0, y: 0});
+    /** @private {!WebGLBuffer} */
+    this._buffer = this._gl.createBuffer();
 
-export default class SpriteGame {
-  constructor(canvas, tiles) {
-    this.canvas = canvas;
-    this.tiles = tiles;
+    /** @private {!Array<!Alloc>} */
+    this._free = [];
 
-    const args = {
-      premultipliedAlpha: false,  // Ask for non-premultiplied alpha
-    };
-    const gl = this.gl = canvas.getContext('webgl', args);
-    if (!gl) {
-      throw new TypeError('no webgl');
-    }
-
-    this._program = new webgl.ShaderProgram(gl, spriteVertexShader, spriteFragmentShader);
-
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
+    /** @private {number} */
     this._index = 0;
+
+    /** @private {number} */
     this._capacity = 0;
-    this._freeIndex = [];
-
-    this._positionData = new Float32Array(0);
-    this._constantData = new Float32Array(0);
-
-    this._spriteBuffer = gl.createBuffer();
-    this._resize(600, false);
-    this._texture = webgl.loadTexture(gl, tiles);
-
-    this._transform = zeroTransform;
   }
 
   /**
-   * @param {?{x: number, y: number}} v
+   * @param {number} size
+   * @export
    */
-  set transform(v) {
-    this._transform = v ? Object.freeze({x: v.x, y: v.y}) : zeroTransform;
-  }
-
- /**
-  * @return {?{x: number, y: number}}
-  */
-  get transform() {
-    return this._transform;
-  }
-
-  /**
-   * @param {number} size of sprite data
-   * @param {boolean} preserve whether to preserve existing data
-   */
-  _resize(size, preserve) {
-    this._capacity = size;
-
-    const verticies = size * offsets.length;
-    this._positionData = new Float32Array(2 * verticies);
-    this._constantData = new Float32Array(this._program.attribSize * verticies);
-
-    const gl = this.gl;
-    const bufferSize =
-        Float32Array.BYTES_PER_ELEMENT * (this._positionData.length + this._constantData.length);
-    gl.bindBuffer(gl.ARRAY_BUFFER, this._spriteBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, bufferSize, gl.DYNAMIC_DRAW);
-
+  resize(size) {
     this._index = 0;
+    this._capacity = size;
+    this._data = new Float32Array(size * this._program.attribSize);
 
-    if (preserve) {
-      throw new Error('preserve not respected yet');
-    }
+    const bufferSize = this._data.length * Float32Array.BYTES_PER_ELEMENT;
+    const gl = this._gl;
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, bufferSize, gl.DYNAMIC_DRAW);
   }
 
   /**
-   * @param {number|undefined} index of sprite
-   * @param {SpriteDef} def of sprite
-   * @return {number} index of sprite
-   * @export
+   * @return {number}
    */
-  update(index, def) {
-    if (index === undefined) {
-      index = this._freeIndex.shift();
-      if (index === undefined) {
-        if (this._index === this._capacity) {
-          throw new Error(`can't add sprite, at capacity: ${this._capacity}`);
-        }
-        index = this._index;
-        ++this._index;
-      }
-    } else if (index < 0 || index >= this._index || this._freeIndex.indexOf(index) !== -1) {
-      throw new Error(`can't update sprite, invalid or free ID: ${index}'`);
-    }
-
-    this._updateAt(index, def);
-    return index;
+  get used() {
+    return this._index;
   }
 
   /**
-   * @param {number} i index of sprite
-   * @param {SpriteDef} def of sprite
-   * @export
+   * Calls `gl.useProgram` and binds all the vertex attributes.
    */
-  _updateAt(i, def) {
-    def = Object.assign({
-      rotation: 0,
-      offset: 0,
-      layer: 0,
-    }, def);
-
-    const spriteSize = 128;
-    const textureSize = 512;
-
-    const vertexBase = i * offsets.length;
-
-    for (let ii = 0; ii < offsets.length; ++ii) {
-      const offset = offsets[ii];
-      const vertexIndex = vertexBase + ii;
-
-      this._positionData[2 * vertexIndex + 0] = def.at.x;
-      this._positionData[2 * vertexIndex + 1] = def.at.y;
-
-      const b = this._program.attribSize * vertexIndex;
-      const s = (attr, off, value) => {
-        this._constantData[b + this._program.info(attr).offset + off] = value;
-      };
-
-      s('rotation', 0, def.rotation);
-      s('offsetY', 0, def.offset);
-      s('layer', 0, def.layer);
-      s('spriteIndex', 0, def.spriteIndex);
-      s('spriteSize', 0, spriteSize);
-      s('cornerOffset', 0, offset[0]);
-      s('cornerOffset', 1, offset[1]);
-      s('spriteTextureSize', 0, spriteSize / textureSize);
-      s('spriteTextureSize', 1, spriteSize / textureSize);
-      s('spritesPerRow', 0, textureSize / spriteSize);
-    }
-
-    const gl = this.gl;
-    gl.bindBuffer(gl.ARRAY_BUFFER, this._spriteBuffer);
-
-    // Upload vertex data.
-    const vertexSub =
-        this._positionData.subarray(vertexBase * 2, (vertexBase + offsets.length) * 2);
-    gl.bufferSubData(gl.ARRAY_BUFFER, vertexBase * 2 * Float32Array.BYTES_PER_ELEMENT, vertexSub);
-
-    // The constant data won't change, so we can immediately upload it.
-    // Remember that the _positionData is at the start of _spriteBuffer.
-    const base = vertexBase * this._program.attribSize;
-    const start = (this._positionData.length + base) * Float32Array.BYTES_PER_ELEMENT;
-    const constantSub =
-        this._constantData.subarray(base, base + offsets.length * this._program.attribSize);
-    gl.bindBuffer(gl.ARRAY_BUFFER, this._spriteBuffer);
-    gl.bufferSubData(gl.ARRAY_BUFFER, start, constantSub);
-  }
-
-  /**
-   * Removes the numbered sprite.
-   * @param {number} v sprite to remove
-   * @export
-   */
-  remove(v) {
-    if (v < 0 || v >= this._index) {
-      return;  // do nothing, out of bounds
-    }
-
-    const alreadyFree = (this._freeIndex.indexOf(v) !== -1);
-    if (alreadyFree) {
-      return;  // already in freeIndex
-    }
-
-    if (v + 1 === this._index) {
-      --this._index;  // if this is the last sprite, just trim buffer
-      return;
-    }
-
-    // TODO(samthor): do something better than this
-    this._updateAt(v, {at: {x: -10000000, y: 0}, spriteIndex: 0});
-    this._freeIndex.push(v);
-  }
-
-  /**
-   * @return {number} the number of sprites that can still be added
-   */
-  get spritesFree() {
-    return this._capacity - this._index + this._freeIndex.length;
-  }
-
-  /**
-   * @export
-   */
-  draw() {
-    const gl = this.gl;
-
-    gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    gl.enable(gl.BLEND);
-    gl.enable(gl.DEPTH_TEST);
-    gl.disable(gl.CULL_FACE);
-    // gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
-    gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
-    // gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
-
-    // Upload just a single texture.
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, this._texture);
-
+  enableAll() {
+    const gl = this._gl;
     gl.useProgram(this._program.program);
-    gl.enableVertexAttribArray(this._program.a('centerPosition'));
-    gl.vertexAttribPointer(this._program.a('centerPosition'), 2, gl.FLOAT, false, 0, 0);
 
-    const base = this._positionData.length;
-    this._program.attribs.forEach((attr) => {
-      if (attr === 'centerPosition') { return; }
+    const attribSize = this._program.attribSize;
+    const attrs = this._program.attrs;
+    for (const key in attrs) {
+      const info = attrs[key];
 
-      const info = this._program.info(attr);
-      if (!info) {
-        throw new Error('expected valid info and loc: ' + attr);
-      }
       gl.enableVertexAttribArray(info.loc);
       gl.vertexAttribPointer(info.loc,
           info.size,
           gl.FLOAT,
           false,
-          this._program.attribSize * Float32Array.BYTES_PER_ELEMENT,  // stride from offset
-          (base + info.offset) * Float32Array.BYTES_PER_ELEMENT);
-    });
-
-    gl.uniform2f(this._program.u('u_transform'), this._transform.x, this._transform.y);
-    gl.uniform2f(this._program.u('u_screenDims'), this.canvas.width, this.canvas.height);
-    gl.uniform1i(this._program.u('u_texture'), 0);
-
-    if (!this._index) {
-      return;  // nothing to draw
+          attribSize * Float32Array.BYTES_PER_ELEMENT,  // stride from offset
+          info.offset * Float32Array.BYTES_PER_ELEMENT);
     }
-    gl.drawArrays(gl.TRIANGLES, 0, this._index * offsets.length);
-  };
-  
+  }
+
+  /**
+   * Allocate a number of verticies.
+   * @param {number} count
+   * @return {!Alloc}
+   */
+  alloc(count) {
+    for (let i = 0; i < this._free.length; ++i) {
+      // FIXME(samthor): For now, we just look for exact matches. Good for fixed size objects.
+      if (this._free[i]._size !== count) {
+        continue;
+      }
+      const found = this._free[i];
+      this._free.splice(i, 1);
+      return found;
+    }
+
+    if (this._index + count >= this._capacity) {
+      throw new TypeError(`can't alloc ${count}, out of space`);
+    }
+
+    const from = this._index;
+    this._index += count;
+    return new Alloc(this, from, count);
+  }
+
+  /**
+   * Frees a previously allocated Alloc. This may leave a gap in the allocation, which will still
+   * be rendered.
+   * @param {!Alloc} alloc
+   */
+  free(alloc) {
+    if (alloc._from + alloc._size === this._capacity) {
+      this._capacity -= alloc._size;
+      // TODO: we may now have an unused Alloc at the end; it doesn't really matter though
+      return;
+    }
+    if (alloc._from > this._capacity) {
+      // ???
+      return;
+    }
+    this._free.push(alloc);
+  }
+
+}
+
+class Alloc {
+  /**
+   * @param {!Renderable} r
+   * @param {number} from
+   * @param {number} size
+   */
+  constructor(r, from, size) {
+    this._r = r;
+    this._from = from;
+    this._size = size;
+    Object.freeze(this);
+  }
+
+  /**
+   * Update this Alloc with new data.
+   *
+   * @param {function(number, !Object<string, function(...number): void>): void} fn
+   */
+  update(fn) {
+    const data = this._r._data;
+    const attribSize = this._r._program.attribSize;
+    const base = attribSize * this._from;
+
+    let at = base;
+    const write = function(off, ...values) {
+      for (let i = 0; i < values.length; ++i) {
+        data[at + off + i] = values[i];
+      }
+    }
+
+    const attrs = this._r._program.attrs;
+
+    /** @type {!Object<string, function(...number): void>} */
+    const updater = {};
+    for (const key in attrs) {
+      updater[key] = write.bind(null, attrs[key].offset);
+    }
+
+    for (let i = 0; i < this._size; ++i) {
+      fn(i, updater);
+      at += attribSize;
+    }
+
+    // reupload whole range
+    const gl = this._r._gl;
+    const sub = data.subarray(base, at);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._r._buffer);
+    gl.bufferSubData(gl.ARRAY_BUFFER, base * Float32Array.BYTES_PER_ELEMENT, sub);
+  }
 }
