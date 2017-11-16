fff46fbfe1dd08b857521280c6d83328e1259096
Le <le@upperquad.com>
2017-11-15 18:00:50 -0800

    Add all stamps

--- scenes/clausedraws/js/tools/images/image-item-candy-jellybean.js
+++ scenes/clausedraws/js/tools/images/image-item-candy-jellybean.js
@@ -27,14 +27,13 @@ goog.require('app.SVGImage');
 app.ImageItemCandyJellybean = function() {
   app.SVGImage.call(this);
 
-  this.width = 44.5;
-  this.height = 59.6;
+  this.width = 26.6;
+  this.height = 14;
 };
 app.ImageItemCandyJellybean.prototype = Object.create(app.SVGImage.prototype);
 
 
 app.ImageItemCandyJellybean.prototype.getSVGString = function(color) {
-  var colors = app.Constants.SVG_COLOR_MATRIX[color];
-  var data = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44.57 59.61"><defs><style>.cls-1{fill:' + colors.primary + ';}.cls-2{fill:' + colors.dark + ';}.cls-3{fill:#fff;}.cls-4{fill:#231f20;opacity:0.1;}.cls-5{fill:#607d8b;}.cls-6{fill:none;stroke:#607d8b;stroke-miterlimit:10;stroke-width:1.03px;}</style></defs><path class="cls-1" d="M25.87,15.63V11.3H18.26v4.35a22.18,22.18,0,1,0,7.61,0Z"/><circle class="cls-2" cx="9.84" cy="27.39" r="1.41"/><circle class="cls-3" cx="16.73" cy="35.71" r="1.41"/><circle class="cls-2" cx="27.56" cy="35.72" r="1.41"/><circle class="cls-1" cx="20.27" cy="28.75" r="1.41"/><circle class="cls-3" cx="29.69" cy="20.85" r="1.41"/><circle class="cls-3" cx="30.38" cy="43.3" r="1.41"/><circle class="cls-3" cx="31.78" cy="30.15" r="1.41"/><circle class="cls-3" cx="18.34" cy="19.82" r="1.41"/><circle class="cls-2" cx="31.1" cy="52.04" r="1.41"/><circle class="cls-2" cx="14.06" cy="44.71" r="1.41"/><circle class="cls-1" cx="39.63" cy="39.94" r="1.41"/><circle class="cls-3" cx="8.43" cy="49.53" r="1.41"/><circle class="cls-1" cx="5.02" cy="35.71" r="1.41"/><circle class="cls-1" cx="20.27" cy="41.6" r="1.41"/><circle class="cls-1" cx="20.95" cy="54.85" r="1.41"/><path class="cls-4" d="M38,21.74a22,22,0,0,0-12.15-6.15V11.3h-3.8v4a24.38,24.38,0,0,1,.17,44.29A22.15,22.15,0,0,0,38,21.74Z"/><path class="cls-5" d="M17.87,8.73a.09.09,0,0,0-.1.1v5.5a.1.1,0,0,0,.17.07l1.22-1.22a.1.1,0,0,1,.14,0l1.32,1.32a.1.1,0,0,0,.14,0l1.32-1.32a.1.1,0,0,1,.14,0l1.32,1.32a.11.11,0,0,0,.15,0L25,13.18a.1.1,0,0,1,.14,0l1.22,1.22a.1.1,0,0,0,.17-.07V8.83a.1.1,0,0,0-.1-.1Z"/><path class="cls-6" d="M22.41,12.25V2.45A1.92,1.92,0,0,0,20.48.52h0a1.93,1.93,0,0,0-1.93,1.93V5.24"/></svg>';
+  var data = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26.62 13.95"><defs><style>.cls-1{fill:' + color + ';}.cls-2{fill:none;stroke:#f2f2f2;stroke-linecap:round;stroke-miterlimit:10;stroke-width:1.69px;opacity:0.3;}</style></defs><path class="cls-1" d="M13.31,14C9.83,14,0,13.11,0,5.71H0A6.11,6.11,0,0,1,.71,2.83,5,5,0,0,1,6.37.31a33.28,33.28,0,0,0,6.94,1.17C17.42,1.48,19.86,0,21.62,0c2.78,0,5,2.62,5,5.71h0C26.62,13.11,16.79,14,13.31,14Z"/><path class="cls-2" d="M3.66,8.33A20.65,20.65,0,0,0,14,10.87c6.34,0,9.52-2.54,9.52-2.54"/></svg>';
   return data;
 };
--- scenes/clausedraws/js/tools/images/image-item-candy-mint-wheel.js
+++ scenes/clausedraws/js/tools/images/image-item-candy-mint-wheel.js
@@ -27,14 +27,13 @@ goog.require('app.SVGImage');
 app.ImageItemCandyMintWheel = function() {
   app.SVGImage.call(this);
 
-  this.width = 44.5;
-  this.height = 59.6;
+  this.width = 28;
+  this.height = 28;
 };
 app.ImageItemCandyMintWheel.prototype = Object.create(app.SVGImage.prototype);
 
 
 app.ImageItemCandyMintWheel.prototype.getSVGString = function(color) {
-  var colors = app.Constants.SVG_COLOR_MATRIX[color];
-  var data = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44.57 59.61"><defs><style>.cls-1{fill:' + colors.primary + ';}.cls-2{fill:' + colors.dark + ';}.cls-3{fill:#fff;}.cls-4{fill:#231f20;opacity:0.1;}.cls-5{fill:#607d8b;}.cls-6{fill:none;stroke:#607d8b;stroke-miterlimit:10;stroke-width:1.03px;}</style></defs><path class="cls-1" d="M25.87,15.63V11.3H18.26v4.35a22.18,22.18,0,1,0,7.61,0Z"/><circle class="cls-2" cx="9.84" cy="27.39" r="1.41"/><circle class="cls-3" cx="16.73" cy="35.71" r="1.41"/><circle class="cls-2" cx="27.56" cy="35.72" r="1.41"/><circle class="cls-1" cx="20.27" cy="28.75" r="1.41"/><circle class="cls-3" cx="29.69" cy="20.85" r="1.41"/><circle class="cls-3" cx="30.38" cy="43.3" r="1.41"/><circle class="cls-3" cx="31.78" cy="30.15" r="1.41"/><circle class="cls-3" cx="18.34" cy="19.82" r="1.41"/><circle class="cls-2" cx="31.1" cy="52.04" r="1.41"/><circle class="cls-2" cx="14.06" cy="44.71" r="1.41"/><circle class="cls-1" cx="39.63" cy="39.94" r="1.41"/><circle class="cls-3" cx="8.43" cy="49.53" r="1.41"/><circle class="cls-1" cx="5.02" cy="35.71" r="1.41"/><circle class="cls-1" cx="20.27" cy="41.6" r="1.41"/><circle class="cls-1" cx="20.95" cy="54.85" r="1.41"/><path class="cls-4" d="M38,21.74a22,22,0,0,0-12.15-6.15V11.3h-3.8v4a24.38,24.38,0,0,1,.17,44.29A22.15,22.15,0,0,0,38,21.74Z"/><path class="cls-5" d="M17.87,8.73a.09.09,0,0,0-.1.1v5.5a.1.1,0,0,0,.17.07l1.22-1.22a.1.1,0,0,1,.14,0l1.32,1.32a.1.1,0,0,0,.14,0l1.32-1.32a.1.1,0,0,1,.14,0l1.32,1.32a.11.11,0,0,0,.15,0L25,13.18a.1.1,0,0,1,.14,0l1.22,1.22a.1.1,0,0,0,.17-.07V8.83a.1.1,0,0,0-.1-.1Z"/><path class="cls-6" d="M22.41,12.25V2.45A1.92,1.92,0,0,0,20.48.52h0a1.93,1.93,0,0,0-1.93,1.93V5.24"/></svg>';
+  var data = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27.9 27.9"><defs><style>.cls-1{fill:#f2f2f2;}.cls-2{fill:' + color + ';}.cls-3{opacity:0.2;}</style></defs><circle class="cls-1" cx="13.95" cy="13.95" r="13.95"/><path class="cls-2" d="M27.42,10.35a14,14,0,0,0-6.49-8.47A13.94,13.94,0,0,0,14,14,13.94,13.94,0,0,1,27.42,10.35Z"/><path class="cls-2" d="M17.55,27.42A14,14,0,0,0,26,20.93,13.93,13.93,0,0,0,14,14,13.94,13.94,0,0,1,17.55,27.42Z"/><path class="cls-2" d="M.49,17.55A14,14,0,0,0,7,26,13.94,13.94,0,0,0,14,14,13.92,13.92,0,0,1,.49,17.55Z"/><path class="cls-2" d="M10.35.49A14,14,0,0,0,1.88,7,13.94,13.94,0,0,0,14,14,13.92,13.92,0,0,1,10.35.49Z"/><path class="cls-3" d="M22.6,3A14,14,0,0,1,3,22.6,14,14,0,1,0,22.6,3Z"/></svg>';
   return data;
 };
