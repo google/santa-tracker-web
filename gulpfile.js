700459d2b9879e795fa50812533b87e0fe485e15
Chris Joel <cdata@google.com>
2017-11-27 13:30:50 -0800

    Include URL polyfill where needed.

--- gulpfile.js
+++ gulpfile.js
@@ -467,6 +467,7 @@ gulp.task('copy-assets', ['bundle', 'build-prod', 'build-prod-manifest'], functi
     'scenes/**/img/**/*.{png,jpg,svg,gif,cur,mp4}',
     'elements/**/img/*.{png,jpg,svg,gif,mp4}',
     'components/webcomponentsjs/*.js',
+    'components/url/*.js',
     'js/ccsender.html',
     // TODO(samthor): Better support for custom scenes (#1679).
     'scenes/snowflake/snowflake-maker/{media,third-party}/**',
