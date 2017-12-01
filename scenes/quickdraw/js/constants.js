c46da7bf16437d7ea82b702ce462ddb6b8d8c69e
Louis <louis.amiot@hetic.net>
2017-12-01 15:57:00 -0800

    Add one word to guess list

--- scenes/quickdraw/js/constants.js
+++ scenes/quickdraw/js/constants.js
@@ -34,7 +34,7 @@ app.Constants = {
   HANDWRITING_RECOGNITION_THRESHOLD: 2.0,
   // Max allowed rate recognition api can be called in seconds
   MAX_API_RATE: 1.0,
-  WORDS: ["campfire", "cookie", "penguin", "snowflake", "snowman", "star", "tree"]
+  WORDS: ["campfire", "cookie", "mug", "penguin", "snowflake", "snowman", "star", "tree"]
 };
 
 
