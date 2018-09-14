c1ba8de067b46530c6a44891de4fbfb9a0ed67a1
Sam Thorogood <sam.thorogood@gmail.com>
2018-09-14 15:09:16 +1000

    Constants safety

--- scenes/shared/js/scoreboard.js
+++ scenes/shared/js/scoreboard.js
@@ -23,6 +23,9 @@ goog.require('app.shared.SharedGameOver');
 // We are *leaking* the Scoreboard global for backwards compatibility.
 app.shared.Scoreboard = Scoreboard;
 
+var Constants =
+    /** @type {{INITIAL_COUNTDOWN: number, COUNTDOWN_FLASH: number}} */ (Constants || {});
+
 /**
  * Manages the scoreboard and game countdown.
  * @constructor
