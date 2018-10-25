fdab8d84a019c7ec490d95177dadd8dfa9fdea50
Sam Thorogood <sam.thorogood@gmail.com>
2018-10-25 16:04:49 +1100

    more fsp wraps

--- build/fsp.js
+++ build/fsp.js
@@ -3,6 +3,8 @@ const util = require('util');
 const mkdirp = require('mkdirp');
 
 module.exports = {
+  copyFile: util.promisify(fs.copyFile),
+  readdir: util.promisify(fs.readdir),
   readFile: util.promisify(fs.readFile),
   writeFile: util.promisify(fs.writeFile),
   mkdirp: util.promisify(mkdirp),
