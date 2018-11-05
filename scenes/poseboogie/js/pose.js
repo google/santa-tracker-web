58e14e4af2821abf1e3b393edbf1462570268c7d
Mark McDonald <macd@google.com>
2018-11-05 14:50:57 +0800

    Removes copied code from posenet demo, importing instead

--- scenes/poseboogie/js/pose.js
+++ scenes/poseboogie/js/pose.js
@@ -1,7 +1,4 @@
-import * as posenet from '@tensorflow-models/posenet';
-
-const color = 'aqua';
-const lineWidth = 2;
+import {drawKeypoints, drawSkeleton} from 'tensorflow-models/posenet/demos/demo_util.js';
 
 /**
  * Feeds an image to posenet to estimate poses - this is where the magic
@@ -44,60 +41,8 @@ export function detectAndDrawPose(video, net, videoWidth, videoHeight) {
       drawSkeleton(pose.keypoints, minPartConfidence, ctx);
     }
 
-    requestAnimationFrame(poseDetectionFrame);
+    window.requestAnimationFrame(poseDetectionFrame);
   }
 
   poseDetectionFrame();
-}
-
-function toTuple({y, x}) {
-  return [y, x];
-}
-
-export function drawPoint(ctx, y, x, r, color) {
-  ctx.beginPath();
-  ctx.arc(x, y, r, 0, 2 * Math.PI);
-  ctx.fillStyle = color;
-  ctx.fill();
-}
-
-/**
- * Draws a line on a canvas, i.e. a joint
- */
-export function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
-  ctx.beginPath();
-  ctx.moveTo(ax * scale, ay * scale);
-  ctx.lineTo(bx * scale, by * scale);
-  ctx.lineWidth = lineWidth;
-  ctx.strokeStyle = color;
-  ctx.stroke();
-}
-
-/**
- * Draw pose keypoints onto a canvas
- */
-export function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
-  for (let i = 0; i < keypoints.length; i++) {
-    const keypoint = keypoints[i];
-
-    if (keypoint.score < minConfidence) {
-      continue;
-    }
-
-    const {y, x} = keypoint.position;
-    drawPoint(ctx, y * scale, x * scale, 3, color);
-  }
-}
-
-/**
- * Draws a pose skeleton by looking up all adjacent keypoints/joints
- */
-export function drawSkeleton(keypoints, minConfidence, ctx, scale = 1) {
-  const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
-      keypoints, minConfidence);
-
-  adjacentKeyPoints.forEach((keypoints) => {
-    drawSegment(toTuple(keypoints[0].position),
-        toTuple(keypoints[1].position), color, scale, ctx);
-  });
 }
