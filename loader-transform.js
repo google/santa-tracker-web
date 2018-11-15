1effd8486b17671dab1ff855c24936eae1f865d5
Sam Thorogood <sam.thorogood@gmail.com>
2018-11-15 16:02:42 +1100

    fix async issues in loader

--- loader-transform.js
+++ loader-transform.js
@@ -51,53 +51,76 @@ function watch(paths, done, timeout) {
 
 
 /**
+ * @param {function(string): ?Object} loader to load file from disk or build it
+ * @param {string} filename to be loaded
+ * @param {function(): void} cleanup to be called once file is done or cannot be loaded
+ * @return {?{body: string, type: ?string}}
+ */
+async function load(loader, filename, cleanup) {
+  const result = {
+    body: null,
+    type: null,
+  };
+  let watching = false;
+
+  try {
+    const raw = await loader(filename);
+    if (!raw) {
+      return null;
+    }
+
+    const {type, cstyle} = defs(filename);
+    result.body = raw.body;
+    result.type = type;
+
+    // bail early if there's no source map, nothing to watch/append
+    if (!raw.map) {
+      return result;
+    }
+
+    // append the encoded source map if possible
+    if (typeof cstyle === 'boolean') {
+      const encodedMap = Buffer.from(JSON.stringify(raw.map), 'utf8').toString('base64');
+      const trailer = `# sourceMappingURL=data:application/json;base64,${encodedMap}`;
+      if (cstyle) {
+        result.body += `/*${trailer} */`;
+      } else {
+        result.body += `//${trailer}`;
+      }
+    }
+
+    // invalidate cache if something observable changes
+    watching = watch(raw.map.sources, cleanup, watchTimeout);
+    return result;
+  } finally {
+    // run cleanup early if it won't be run by watch
+    watching || cleanup();
+  }
+
+  throw new Error('should not get here');
+}
+
+
+/**
  * Builds a Koa transform which wraps the given Loader.
  */
 module.exports = function(loader) {
   const cached = {};
 
-  function prepare(filename, out) {
-    const {type, cstyle} = defs(filename);
-    const result = {body: out.body, type};
-
-    if (out.map) {
-      // invalidate cache if something observable changes
-      const cleanup = () => delete cached[filename];
-      if (watch(out.map.sources, cleanup, watchTimeout)) {
-        cached[filename] = result;
-      }
-
-      // append the encoded sourceMap
-      if (typeof cstyle === 'boolean') {
-        const encodedMap = Buffer.from(JSON.stringify(out.map), 'utf8').toString('base64');
-        const trailer = `# sourceMappingURL=data:application/json;base64,${encodedMap}`;
-        if (cstyle) {
-          result.body += `/*${trailer} */`;
-        } else {
-          result.body += `//${trailer}`;
-        }
-      }
-    }
-
-    return result;
-  }
-
   return async (ctx, next) => {
     const filename = ctx.path.substr(1);
 
-    let result = cached[filename];
-    if (result === undefined) {
-      // nothing cached, call the actual loader
-      const out = await loader(filename);
-      if (out === null) {
-        return next();
-      }
+    let p = cached[filename];
+    if (p === undefined) {
+      // there was no cached result: fetch one, and delete it on cleanup (or if we can't retain
+      // the result, e.g., nothing sane can be watched)
+      p = load(loader, filename, () => delete cached[filename]);
+      cached[filename] = p;
+    }
 
-      if (cached[filename] !== undefined) {
-        result = cached[filename];  // another request 'won'
-      } else {
-        result = prepare(filename, out);
-      }
+    const result = await p;
+    if (result === null) {
+      return next();
     }
 
     ctx.response.body = result.body;
