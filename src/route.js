701485a13bad54edc117867abd8b1762efa0a25c
Sam Thorogood <sam.thorogood@gmail.com>
2018-11-17 16:51:15 +1100

    comments/remove debugger

--- src/route.js
+++ src/route.js
@@ -2,15 +2,19 @@ import * as dom from './lib/dom.js';
 
 const simplePathMatcher = /^\/?(?:|(\w+)\.html)$/;
 
-
+/**
+ * @return {string} prod scope
+ */
 function determineScope() {
   const scope = document.body.getAttribute('data-scope');
   return scope || 'https://santatracker.google.com/';
 }
 
-
+/**
+ * @param {!Event} ev to read from
+ * @return {?string} found scene name based on scope
+ */
 export function fromClick(ev) {
-  debugger;
   const url = dom.urlFromClickEvent(ev);
   if (url === null) {
     return null;
@@ -33,6 +37,10 @@ export function fromClick(ev) {
   }
 }
 
+/**
+ * @param {string} cand candidate href
+ * @return {string} href with scope as appropriate
+ */
 export function href(cand) {
   try {
     const url = new URL(cand);
@@ -45,11 +53,14 @@ export function href(cand) {
     const url = new URL(cand, scope);
     return url.toString();
   } catch (e) {
-    // ???
+    return cand;  // give up
   }
-  return cand;  // totally give up
 }
 
+/**
+ * @param {string} sceneName
+ * @return {string} public URL to scene name
+ */
 export function scene(sceneName) {
   const scope = determineScope();
   if (sceneName) {
