5948bd6a3a71df900b1c41ad941ebe4c0d30a81c
Luke Mahe <lukem@google.com>
2018-11-27 16:15:16 +1100

    adding wrangler

---
+++ scenes/index/elements/wrangler.js
@@ -0,0 +1,30 @@
+import {html, LitElement} from '@polymer/lit-element';
+
+export class ReindeerWranglerElement extends LitElement {
+  render() {
+    return html`
+    <style>${_style`wrangler`}</style>
+    <div id="holder">
+      <div id="wrangler">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div id="reindeer1">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div id="reindeer2">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+    `;
+  }
+}
+
+customElements.define('reindeer-wrangler', ReindeerWranglerElement);
