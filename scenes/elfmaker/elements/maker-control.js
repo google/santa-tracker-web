dc32406fcdd6c42e619b18c1294c2f07952f019e
Andreas Jeppsson <andreas@plan8.se>
2018-11-28 13:42:13 +0100

    no sound on first elf update event

--- scenes/elfmaker/elements/maker-control.js
+++ scenes/elfmaker/elements/maker-control.js
@@ -95,6 +95,7 @@ ${renderClass('accessories', 'fill', this.accessoriesColor)}
     return super.update(changedProperties);
   }
   playChangeSound(changedProperties) {
+    if (changedProperties.has('_idPrefix')) return; //first event, play no sound
     if (changedProperties.has('category')) {
       window.santaApp.fire('sound-trigger', 'elfmaker_switch_type');
     }else if (changedProperties.has('categoryChoice')) {
