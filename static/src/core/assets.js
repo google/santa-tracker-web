7ffec9188979f55c0e1a8a8298cf9e4a857f6375
Sam Thorogood <sam.thorogood@gmail.com>
2019-09-09 15:05:16 +1000

    add missing _static

--- static/src/core/assets.js
+++ static/src/core/assets.js
@@ -1,4 +1,6 @@
 
+import {_static} from '../magic.js';
+
 /**
  * @param {string} sceneName to load a feature image for
  * @return {!Promise<!Image>}
@@ -9,10 +11,10 @@ export function sceneImage(sceneName) {
     return Promise.reject('invalid scene name');
   }
 
-  const base = join(import.meta.url, '../../img/scenes/') + sceneName;
+  const base = _static`img/scenes/` + sceneName;
   const img = document.createElement('img');
   img.src = base + (window.devicePixelRatio > 1 ? '_2x.png' : '_1x.png');
-  img.srcset = `${base}_1x.png, ${img.src}_2x.png 2x`;
+  img.srcset = `${base}_1x.png, ${base}_2x.png 2x`;
 
   return new Promise((resolve, reject) => {
     img.onload = () => resolve(img);
