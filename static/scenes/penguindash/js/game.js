d1697c10f2891b201b3c8f482b02659abba0d9d7
Sam Thorogood <sam.thorogood@gmail.com>
2019-09-17 16:55:32 +1000

    penguindash scene

---
+++ static/scenes/penguindash/js/game.js
@@ -0,0 +1,313 @@
+/*
+ * Copyright 2015 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+goog.provide('app.Game');
+
+goog.require('app.Constants');
+goog.require('app.Preloader');
+goog.require('app.Start');
+goog.require('app.shared.LevelUp');
+goog.require('app.shared.Gameover');
+goog.require('app.shared.Scoreboard');
+goog.require('app.shared.Tutorial');
+
+
+
+/**
+ * Main game class.
+ * @param {!Element} elem DOM element containing the game.
+ * @constructor
+ * @export
+ */
+app.Game = function(elem, componentDir) {
+  this.startPromise = new Promise((resolve) => {
+    this.resolveStart = resolve;
+  });
+  this.startPromise.then(() => {
+    this.updateSize_();
+  });
+
+  this._realElem = elem;
+  this.elem = $(elem);
+  this.componentDir = componentDir;
+
+  this.sceneElem = this.elem.find('#penguindash-game');
+
+  this.level = 1;
+  this.sceneSize = { width: 0, height: 0 };
+  this.entities = [];
+  this.isPlaying = false;
+  this.paused = false;
+  this.hidden = false;
+  this.scale = 1;
+  this.gameStartTime = +new Date;
+  this.watchSceneSize_();
+  this.watchOrientation_();
+
+  this.scoreboard = new app.shared.Scoreboard(this, this.elem.find('.board'), app.Constants.TOTAL_LEVELS);
+  this.levelUp = new app.shared.LevelUp(this, this.elem.find('.levelup'), this.elem.find('.levelup--number'));
+  this.gameoverView = new app.shared.Gameover(this, this.elem.find('.gameover'));
+  this.tutorial = new app.shared.Tutorial(this.elem, 'device-tilt keys-arrows spacenav-arrows');
+
+  this.gyroPresent = false;
+  var detectGyro = function(event){
+    if (!this.gyroPresent) {
+      var e = event.originalEvent;
+      if (e.alpha || e.beta || e.gamma) {
+        this.gyroPresent = true;
+      }
+      this.showLockscreenMessage();
+    }
+  }.bind(this);
+  $(window).on('deviceorientation.penguindash', function(event){
+    detectGyro(event);
+  });
+
+  this.initGame = this.initGame.bind(this);
+  this.initGame(true);
+};
+
+
+/**
+ * Starts the game. Should only be called once.
+ */
+app.Game.prototype.start = function() {
+  this.tutorial.start();
+
+  //in case the game hasn't been fully loaded
+  this.game.penguinPauseOnStart = false;
+  this.game.paused = false;
+
+  this.scoreboard.reset();
+  this.scoreboard.restart();
+
+
+  // make sure canvas is real size
+  const canvas = this._realElem.querySelector('canvas');
+  function checkSize() {
+
+    if (canvas.width && canvas.height) {
+      return;  // great
+    }
+    window.dispatchEvent(new Event('resize'));  // kick phaser
+
+    window.requestAnimationFrame(checkSize);
+  }
+  checkSize();
+};
+
+
+/**
+ * Restarts the game. Can be called at any time.
+ */
+app.Game.prototype.restart = function() {
+  this.initGame(false);
+
+  this.scoreboard.reset();
+  this.scoreboard.restart();
+};
+
+/**
+ * Initialize the game to speed up start time.
+ @param {boolean} pause Indicate if the game should pause after initialization.
+ */
+app.Game.prototype.initGame = function(pause) {
+  window.PhaserGlobal = {};
+  window.PhaserGlobal.disableAudio = true;
+
+  if(this.game) {
+    this.game.destroy();
+  }
+
+  this.game = new Phaser.Game('100%', '100%', Phaser.AUTO, this.sceneElem[0], null, true);
+  this.game.st_parent = this;
+
+  this.game.penguinPauseOnStart = pause;
+
+  this.game.state.add('Preloader', app.Preloader);
+  this.game.state.add('Game', app.Start);
+  this.game.state.start('Preloader');
+};
+
+
+/**
+ * Scale the game down for smaller resolutions.
+ * @param {number} scale A scale between 0 and 1 on how much to scale.
+ */
+app.Game.prototype.setScale = function(scale) {
+  this.scale = scale;
+  if (this.game && this.game.world) {
+    this.game.setScale(scale);
+  }
+};
+
+
+/**
+ * Freezes the game. Stops the onFrame loop and stops any CSS3 animations.
+ * Used both for game over and pausing.
+ */
+app.Game.prototype.freezeGame = function() {
+  this.isPlaying = false;
+  this.elem.addClass('frozen');
+  if (this.game) {
+    this.game.pause();
+  }
+};
+
+
+/**
+ * Unfreezes the game.
+ */
+app.Game.prototype.unfreezeGame = function() {
+  if (!this.isPlaying) {
+    this.isPlaying = true;
+    this.elem.removeClass('frozen').focus();
+    if (this.game) {
+      this.game.unpause();
+    }
+  }
+};
+
+
+/**
+ * Stops the game as game over. Displays the game over screen as well.
+ */
+app.Game.prototype.gameover = function() {
+  this.freezeGame();
+  this.gameoverView.show();
+  window.santaApp.fire('sound-trigger', 'music_ingame_gameover');
+  window.santaApp.fire('analytics-track-game-over', {
+    gameid: 'penguindash',
+    score: this.scoreboard.score,
+    level: this.level,
+    timePlayed: new Date - this.gameStartTime
+  });
+};
+
+
+/**
+ * Pause the game.
+ */
+app.Game.prototype.pause = function() {
+  this.paused = true;
+  this.freezeGame();
+};
+
+
+/**
+ * Resume the game.
+ */
+app.Game.prototype.resume = function() {
+  this.paused = false;
+  this.unfreezeGame();
+};
+
+/**
+ * Show Lockscreen Message.
+ */
+app.Game.prototype.showLockscreenMessage = function () {
+  if (this.gyroPresent){
+    var paused = this.paused;
+    var lockElem = this.elem.find('.lockscreen');
+    if (lockElem.attr('hidden')) {
+      return;  // do nothing
+    }
+    if (!paused) {
+      this.pause();
+    }
+    lockElem.removeClass('hidden');
+    window.setTimeout(() => {
+      lockElem.addClass('hidden');
+      if (!paused) {
+        this.resume();
+      }
+    }, 3000);
+  }
+};
+
+
+app.Game.prototype.updateSize_ = function() {
+  var width = window.innerWidth,
+    height = window.innerHeight - window.santaApp.headerSize,
+    scale = width < 980 ? (width + 490) / (980 + 490) : 1;
+  this.setScale(scale);
+  console.info('got size', scale);
+
+  this.sceneSize.height = height * (1 / this.scale);
+  this.sceneSize.width = width * (1 / this.scale);
+
+  const canvas = this._realElem.querySelector('canvas');
+  if (canvas) {
+    canvas.focus();
+  } else {
+    this._realElem.focus();
+  }
+
+  this.showLockscreenMessage();
+};
+
+
+/**
+ * Manages a cache of the scene size. Updates on window resize.
+ * @private
+ */
+app.Game.prototype.watchSceneSize_ = function() {
+  this.updateSize_();
+  $(window).on('resize.penguindash', this.updateSize_.bind(this));
+};
+
+
+/**
+ * Manages device orientation. Updates on orientation change.
+ * @private
+ */
+app.Game.prototype.watchOrientation_ = function() {
+  // var updateOrientation = function() {
+  //   if ((screen.orientation && screen.orientation.angle !== 0) ||
+  //     (window.orientation && window.orientation !== 0)) {
+  //     this.pause();
+  //   }
+  //   else {
+  //     this.resume();
+  //   }
+  // }.bind(this);
+
+  // updateOrientation();
+  // $(window).on('orientationchange.penguindash', updateOrientation);
+};
+
+
+/**
+ * Cleanup
+ * @export
+ */
+app.Game.prototype.dispose = function() {
+  if (this.isPlaying) {
+    window.santaApp.fire('analytics-track-game-quit', {
+      gameid: 'penguindash',
+      timePlayed: new Date - this.gameStartTime,
+      level: this.level
+    });
+  }
+  this.freezeGame();
+
+  $(window).off('.penguindash');
+  $(document).off('.penguindash');
+  this.elem.off('.penguindash');
+
+  this.tutorial.dispose();
+};
+
---
+++ static/scenes/penguindash/js/preloader.js
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2015 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+goog.provide('app.Preloader');
+goog.provide('Preloader');
+
+
+app.Preloader = function(game) {};
+
+app.Preloader.prototype.preload = function() {
+  this.load.crossOrigin = 'anonymous';
+  this.load.baseURL = this.game.st_parent.componentDir;
+
+  // nb. set asBlob to true for CORS
+  this.load.video('background', 'img/wave-bg-slow-big-no-waves.mp4', 'canplaythrough', true);
+
+  this.load.spritesheet('sprite-penguin', 'img/sprite-penguin.png', 153, 108, 32);
+  this.load.image('shadow-penguin', 'img/penguin-shadow.png');
+  this.load.spritesheet('sprite-dust', 'img/sprite-dust.png', 150, 108, 16);
+
+  this.load.image('element-present1', 'img/element-present1.png');
+  this.load.image('element-present2', 'img/element-present2.png');
+  this.load.image('element-present3', 'img/element-present3.png');
+  this.load.image('element-present4', 'img/element-present4.png');
+  this.load.image('element-present5', 'img/element-present5.png');
+  this.load.image('element-present6', 'img/element-present6.png');
+  this.load.image('element-presentStacked', 'img/element-presentStacked.png');
+
+  this.load.image('element-coal1', 'img/element-coal1.png');
+  this.load.image('element-coal2', 'img/element-coal2.png');
+  this.load.image('element-coal3', 'img/element-coal3.png');
+  this.load.image('element-tree1', 'img/element-tree1.png');
+  this.load.image('element-tree2', 'img/element-tree2.png');
+
+  this.load.spritesheet('sprite-duck', 'img/sprite-duck.png', 169, 202, 22);
+  this.load.spritesheet('sprite-green_elf', 'img/sprite-green_elf.png', 169, 202, 17);
+  this.load.spritesheet('sprite-orange_elf', 'img/sprite-orange_elf.png', 169, 202, 16);
+  this.load.spritesheet('sprite-walrus', 'img/sprite-walrus.png', 169, 202, 17);
+  this.load.spritesheet('sprite-angels', 'img/sprite-angels.png', 900, 750, 1);
+  this.load.spritesheet('sprite-sm_iceberg', 'img/sprite-sm_iceberg.png', 720, 480, 1);
+  this.load.spritesheet('sprite-iceberg', 'img/sprite-iceberg.png', 1200, 900, 1);
+  this.load.spritesheet('sprite-snowman', 'img/sprite-snowman.png', 736, 616, 1);
+
+  // still image fallback for mobile
+  this.load.spritesheet('element-duck', 'img/element-duck.png');
+  this.load.spritesheet('element-green_elf', 'img/element-green_elf.png');
+  this.load.spritesheet('element-orange_elf', 'img/element-orange_elf.png');
+  this.load.spritesheet('element-walrus', 'img/element-walrus.png');
+  this.load.spritesheet('element-angels', 'img/element-angels.png');
+  this.load.spritesheet('element-sm_iceberg', 'img/element-sm_iceberg.png');
+  this.load.spritesheet('element-iceberg', 'img/element-iceberg.png');
+  this.load.spritesheet('element-snowman', 'img/element-snowman.png');
+
+  this.load.image('element-snow1', 'img/element-snow1.png');
+  this.load.image('element-snow1-shadow', 'img/element-snow1-shadow.png');
+  this.load.image('element-snow2', 'img/element-snow2.png');
+  this.load.image('element-snow2-shadow', 'img/element-snow2-shadow.png');
+  this.load.image('element-snow3', 'img/element-snow3.png');
+  this.load.image('element-snow3-shadow', 'img/element-snow3-shadow.png');
+  this.load.image('element-snow4', 'img/element-snow4.png');
+  this.load.image('element-snow4-shadow', 'img/element-snow4-shadow.png');
+  this.load.image('element-snow5', 'img/element-snow5.png');
+  this.load.image('element-snow5-shadow', 'img/element-snow5-shadow.png');
+  this.load.image('element-ice1', 'img/element-ice1.png');
+  this.load.image('element-ice1-shadow', 'img/element-ice1-shadow.png');
+  this.load.image('element-ice2', 'img/element-ice2.png');
+  this.load.image('element-ice2-shadow', 'img/element-ice2-shadow.png');
+  this.load.image('element-ice3', 'img/element-ice3.png');
+  this.load.image('element-ice3-shadow', 'img/element-ice3-shadow.png');
+  this.load.image('element-ice4', 'img/element-ice4.png');
+  this.load.image('element-ice4-shadow', 'img/element-ice4-shadow.png');
+  this.load.image('element-ice5', 'img/element-ice5.png');
+  this.load.image('element-ice5-shadow', 'img/element-ice5-shadow.png');
+  this.load.image('element-finish', 'img/element-finish.png');
+
+  this.load.image('element-start', 'img/element-start.png');
+  this.load.image('element-finish', 'img/element-finish.png');
+  this.load.image('element-celebrate', 'img/element-celebrate.png');
+  this.load.image('element-pole', 'img/element-pole.png');
+
+  this.load.spritesheet('sprite-wave1', 'img/sprite-wave1.png', 250, 100, 41);
+  this.load.spritesheet('sprite-wave2', 'img/sprite-wave2.png', 250, 100, 55);
+};
+
+
+app.Preloader.prototype.create = function() {
+  this.game.state.start('Game');
+};
