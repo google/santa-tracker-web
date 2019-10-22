07d0da6d296a789bc7a979a6f05fa933d09f91b1
Robpayot <robpayot@gmail.com>
2019-10-21 17:31:00 -0700

    saving point

--- static/scenes/snowbox/js/components/SceneSubjects/PineTree/index.js
+++ static/scenes/snowbox/js/components/SceneSubjects/PineTree/index.js
@@ -33,41 +33,41 @@ class PineTree extends Obj {
 
     // CANNONJS PART
 
-    const bunnyBody = new CANNON.Body({ mass: this.mass });
+    // const bunnyBody = new CANNON.Body({ mass: this.mass });
 
-    // console.log(geometry, modelJSON)
+    // // console.log(geometry, modelJSON)
 
-    var rawVerts = modelJSON.vertices;
-    var rawFaces = modelJSON.faces;
-    // var rawOffset = bunny[i].offset;
+    // var rawVerts = modelJSON.vertices;
+    // var rawFaces = modelJSON.faces;
+    // // var rawOffset = bunny[i].offset;
 
-    var verts=[], faces=[], offset;
+    // var verts=[], faces=[], offset;
 
-    // Get vertices
-    for(var j=0; j< rawVerts.length; j+=3){
-        verts.push(new CANNON.Vec3( rawVerts[j]  ,
-                                    rawVerts[j+1],
-                                    rawVerts[j+2]));
-    }
+    // // Get vertices
+    // for(var j=0; j< rawVerts.length; j+=3){
+    //     verts.push(new CANNON.Vec3( rawVerts[j]  ,
+    //                                 rawVerts[j+1],
+    //                                 rawVerts[j+2]));
+    // }
 
-    // Get faces
-    for(var j=0; j<rawFaces.length; j+=3){
-        faces.push([rawFaces[j],rawFaces[j+1],rawFaces[j+2]]);
-    }
+    // // Get faces
+    // for(var j=0; j<rawFaces.length; j+=3){
+    //     faces.push([rawFaces[j],rawFaces[j+1],rawFaces[j+2]]);
+    // }
 
-    // Get offset
-    // offset = new CANNON.Vec3(rawOffset[0],rawOffset[1],rawOffset[2]);
+    // // Get offset
+    // // offset = new CANNON.Vec3(rawOffset[0],rawOffset[1],rawOffset[2]);
 
-    // Construct polyhedron
-    var bunnyPart = new CANNON.ConvexPolyhedron(verts, faces);
+    // // Construct polyhedron
+    // var bunnyPart = new CANNON.ConvexPolyhedron(verts, faces);
 
-    // Add to compound
-    bunnyBody.addShape(bunnyPart, offset);
+    // // Add to compound
+    // bunnyBody.addShape(bunnyPart, offset);
 
-    console.log(bunnyBody)
+    // console.log(bunnyBody)
 
 
-    // for(var i=0; i< geometry.length; i++){
+    // for(var i=0; i< bunny.length; i++){
 
     //     var rawVerts = bunny[i].verts;
     //     var rawFaces = bunny[i].faces;
@@ -97,14 +97,14 @@ class PineTree extends Obj {
     //     bunnyBody.addShape(bunnyPart,offset);
     // }
 
-    // const shape = new CANNON.Box(new CANNON.Vec3(CONFIG.SIZE / 2, CONFIG.SIZE / 2, CONFIG.SIZE / 2))
-    // this.body = new CANNON.Body({
-    //   mass: this.mass,
-    //   shape,
-    //   fixedRotation: false,
-    //   material: type === 'ice' ? GLOBAL_CONFIG.SLIPPERY_MATERIAL : GLOBAL_CONFIG.NORMAL_MATERIAL
-    // })
-    this.body = bunnyBody
+    const shape = new CANNON.Box(new CANNON.Vec3(CONFIG.SIZE / 2, CONFIG.SIZE / 2, CONFIG.SIZE / 2))
+    this.body = new CANNON.Body({
+      mass: this.mass,
+      shape,
+      fixedRotation: false,
+      material: type === 'ice' ? GLOBAL_CONFIG.SLIPPERY_MATERIAL : GLOBAL_CONFIG.NORMAL_MATERIAL
+    })
+    // this.body = bunnyBody
     this.body.position.set(-CONFIG.SIZE / 2, 100, -CONFIG.SIZE / 2)
 
     // Mesh
