59994d1bf81f1d82b6339e6c5cf02aa2af8ce4e6
Louis Amiot <louis.amiot92@gmail.com>
2019-10-23 19:16:32 +0200

    Update tree

--- static/scenes/snowbox/js/components/SceneSubjects/Tree/index.js
+++ static/scenes/snowbox/js/components/SceneSubjects/Tree/index.js
@@ -4,23 +4,26 @@ import Obj from '../../Object/index.js'
 import GLOBAL_CONFIG from '../../SceneManager/config.js'
 import CONFIG from './config.js'
 
-let cubeGeo, cubeMaterial
+let treeGeo, treeMaterial
 
 const textureLoader = new THREE.TextureLoader()
 const model = './models/5_tree.obj'
-const normalMap = textureLoader.load('./models/1_cube.jpg')
+const material = './models/5_tree.mtl'
 
 // preload objs
-new THREE.OBJLoader().load(model, object => {
-  cubeGeo = object.children[0].geometry
-  cubeGeo.center()
-  cubeMaterial = new THREE.MeshToonMaterial({
-    color: GLOBAL_CONFIG.COLORS.ICE,
-    shininess: 345,
-    normalMap
-  })
+new THREE.MTLLoader().load(material, materials => {
+  materials.preload()
+  treeMaterial = materials.materials.Mat
+  treeMaterial.color.set(GLOBAL_CONFIG.COLORS.ICE)
+  treeMaterial.shininess = 345
+  treeMaterial.needsUpdate = true
 
-  cubeMaterial.needsUpdate = true
+  const loader = new THREE.OBJLoader()
+  loader.setMaterials(materials)
+  loader.load(model, object => {
+    treeGeo = object.children[0].geometry
+    treeGeo.center()
+  })
 })
 
 class Cube extends Obj {
@@ -30,9 +33,9 @@ class Cube extends Obj {
 
     this.selectable = CONFIG.SELECTABLE
     this.mass = CONFIG.MASS
-    this.defaultMaterial = cubeMaterial
+    this.defaultMaterial = treeMaterial
 
-    this.shapeHeight = (cubeGeo.boundingBox.max.y - cubeGeo.boundingBox.min.y) * (1 / GLOBAL_CONFIG.MODEL_UNIT)
+    this.shapeHeight = (treeGeo.boundingBox.max.y - treeGeo.boundingBox.min.y) * (1 / GLOBAL_CONFIG.MODEL_UNIT)
 
     const shape = new CANNON.Box(new CANNON.Vec3(CONFIG.SIZE / 2, this.shapeHeight / 2, CONFIG.SIZE / 2))
     this.body = new CANNON.Body({
@@ -44,7 +47,7 @@ class Cube extends Obj {
     this.body.position.set(-CONFIG.SIZE / 2, 0, -CONFIG.SIZE / 2)
 
     // Mesh
-    this.mesh = new THREE.Mesh(cubeGeo, cubeMaterial)
+    this.mesh = new THREE.Mesh(treeGeo, treeMaterial)
     this.mesh.scale.multiplyScalar(1 / GLOBAL_CONFIG.MODEL_UNIT)
     this.mesh.updateMatrix()
 
