10a434557e582eb3f50088d2edb1773d0530b18d
Robpayot <robpayot@gmail.com>
2019-10-25 12:08:55 +1100

    load wrl

--- static/scenes/snowbox/js/components/SceneSubjects/PineTree/index.js
+++ static/scenes/snowbox/js/components/SceneSubjects/PineTree/index.js
@@ -1,4 +1,6 @@
 import Obj from '../../Object/index.js'
+import WRLLoader from '../../../managers/WRLLoader.js'
+import { generateBody } from '../../../utils/createCollisionBodies.js'
 
 // Config
 import GLOBAL_CONFIG from '../../SceneManager/config.js'
@@ -6,11 +8,17 @@ import CONFIG from './config.js'
 
 import modelJSON from '../../../../models/pine-tree_v02.json'
 
-let geometry, material
+let geometry, material, finalObject
 
 const textureLoader = new THREE.TextureLoader()
 const model = './models/pine-tree_v01.obj'
+const wrl = './models/pine-tree_v01.wrl'
 // const normalMap = textureLoader.load('./models/shape_03--normal.jpg')
+
+new WRLLoader().load(wrl).then(object => {
+    console.log(object)
+    finalObject = object
+})
 
 // preload objs
 new THREE.OBJLoader().load(model, object => {
@@ -33,78 +41,16 @@ class PineTree extends Obj {
 
     // CANNONJS PART
 
-    const bunnyBody = new CANNON.Body({ mass: this.mass });
-
-    // console.log(geometry, modelJSON)
-
-    var rawVerts = modelJSON.vertices;
-    var rawFaces = modelJSON.faces;
-    // var rawOffset = bunny[i].offset;
-
-    var verts=[], faces=[], offset;
-
-    // Get vertices
-    for(var j=0; j< rawVerts.length; j+=3){
-        verts.push(new CANNON.Vec3( rawVerts[j]  ,
-                                    rawVerts[j+1],
-                                    rawVerts[j+2]));
-    }
-
-    // Get faces
-    for(var j=0; j<rawFaces.length; j+=3){
-        faces.push([rawFaces[j],rawFaces[j+1],rawFaces[j+2]]);
-    }
-
-    // Get offset
-    // offset = new CANNON.Vec3(rawOffset[0],rawOffset[1],rawOffset[2]);
-
-    // Construct polyhedron
-    var bunnyPart = new CANNON.ConvexPolyhedron(verts, faces);
-
-    // Add to compound
-    bunnyBody.addShape(bunnyPart, offset);
-
-    // console.log(bunnyBody)
-
-
-    // for(var i=0; i< bunny.length; i++){
-
-    //     var rawVerts = bunny[i].verts;
-    //     var rawFaces = bunny[i].faces;
-    //     var rawOffset = bunny[i].offset;
-
-    //     var verts=[], faces=[], offset;
-
-    //     // Get vertices
-    //     for(var j=0; j<rawVerts.length; j+=3){
-    //         verts.push(new CANNON.Vec3( rawVerts[j]  ,
-    //                                     rawVerts[j+1],
-    //                                     rawVerts[j+2]));
-    //     }
-
-    //     // Get faces
-    //     for(var j=0; j<rawFaces.length; j+=3){
-    //         faces.push([rawFaces[j],rawFaces[j+1],rawFaces[j+2]]);
-    //     }
-
-    //     // Get offset
-    //     offset = new CANNON.Vec3(rawOffset[0],rawOffset[1],rawOffset[2]);
-
-    //     // Construct polyhedron
-    //     var bunnyPart = new CANNON.ConvexPolyhedron(verts,faces);
-
-    //     // Add to compound
-    //     bunnyBody.addShape(bunnyPart,offset);
-    // }
-
-    const shape = new CANNON.Box(new CANNON.Vec3(CONFIG.SIZE / 2, CONFIG.SIZE / 2, CONFIG.SIZE / 2))
-    this.body = new CANNON.Body({
-      mass: this.mass,
-      shape,
-      fixedRotation: false,
-      material: type === 'ice' ? GLOBAL_CONFIG.SLIPPERY_MATERIAL : GLOBAL_CONFIG.NORMAL_MATERIAL
-    })
-    // this.body = bunnyBody
+    this.body = generateBody(finalObject, { mass: CONFIG.MASS, scale: 10 })
+    // const shape = new CANNON.Box(new CANNON.Vec3(CONFIG.SIZE / 2, CONFIG.SIZE / 2, CONFIG.SIZE / 2))
+    // this.body = new CANNON.Body({
+    //   mass: this.mass,
+    //   shape,
+    //   fixedRotation: false,
+    //   material: type === 'ice' ? GLOBAL_CONFIG.SLIPPERY_MATERIAL : GLOBAL_CONFIG.NORMAL_MATERIAL
+    // })
+    console.log(this.body)
+    // console.log(this.body2)
     this.body.position.set(-CONFIG.SIZE / 2, 100, -CONFIG.SIZE / 2)
 
     // Mesh
@@ -117,12 +63,13 @@ class PineTree extends Obj {
 
   scaleBody() {
     const shape = this.body.shapes[0]
-    shape.halfExtents.set(
-      (CONFIG.SIZE / 2) * this.scaleFactor,
-      (CONFIG.SIZE / 2) * this.scaleFactor,
-      (CONFIG.SIZE / 2) * this.scaleFactor
-    )
-    shape.updateConvexPolyhedronRepresentation()
+    console.log(shape)
+    // shape.halfExtents.set(
+    //   (CONFIG.SIZE / 2) * this.scaleFactor,
+    //   (CONFIG.SIZE / 2) * this.scaleFactor,
+    //   (CONFIG.SIZE / 2) * this.scaleFactor
+    // )
+    // shape.updateConvexPolyhedronRepresentation()
     console.log(CONFIG.MASS * Math.pow(CONFIG.SIZE * this.scaleFactor, 3))
     this.body.mass = CONFIG.MASS * Math.pow(CONFIG.SIZE * this.scaleFactor, 3)
     this.body.computeAABB()
