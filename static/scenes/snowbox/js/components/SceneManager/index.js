6135eb5082ddb54c26cec92de8b86fb0c0109c3b
Robpayot <robpayot@gmail.com>
2019-10-30 18:48:32 +1100

    setup colors

--- static/scenes/snowbox/js/components/SceneManager/index.js
+++ static/scenes/snowbox/js/components/SceneManager/index.js
@@ -373,11 +373,27 @@ class SceneManager extends EventEmitter {
         el.classList.add('is-open')
         break
       case 'pick-color':
-        // console.log(parseInt(el.dataset.color, 16))
-        // console.log(this.selectedSubject, this.activeSubject)
         if (this.activeSubject) {
-          this.activeSubject.mesh.material.color = new THREE.Color('#d2d2d2')
+          console.log(this.activeSubject)
+          this.activeSubject.mesh.material.color = new THREE.Color(el.dataset.color)
           this.activeSubject.mesh.material.needsUpdate = true
+          /* Resta el porcentaje indicado a un color (RR, GG o BB) hexadecimal para oscurecerlo */
+          const subtractLight = function(color, amount){
+            let cc = parseInt(color,16) - amount;
+            let c = (cc < 0) ? 0 : (cc);
+            c = (c.toString(16).length > 1 ) ? c.toString(16) : `0${c.toString(16)}`;
+            return c;
+          }
+
+          /* Oscurece un color hexadecimal de 6 caracteres #RRGGBB segun el porcentaje indicado */
+          const darken = (color, amount) =>{
+            color = (color.indexOf("#")>=0) ? color.substring(1,color.length) : color;
+            amount = parseInt((255*amount)/100);
+            return color = `#${subtractLight(color.substring(0,2), amount)}${subtractLight(color.substring(2,4), amount)}${subtractLight(color.substring(4,6), amount)}`;
+          }
+
+          this.activeSubject.materials.highlight.color = new THREE.Color(darken(el.dataset.color, 15))
+          this.activeSubject.materials.highlight.needsUpdate = true
         }
         break
       default:
