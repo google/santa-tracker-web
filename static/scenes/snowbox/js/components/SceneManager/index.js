0e3352eca112b6d0bf722becbfb069a71ff9fbd0
Robpayot <robpayot@gmail.com>
2019-10-30 21:42:24 +1100

    remove is-open class

--- static/scenes/snowbox/js/components/SceneManager/index.js
+++ static/scenes/snowbox/js/components/SceneManager/index.js
@@ -1,5 +1,6 @@
 import { EventEmitter } from '../../event-emitter.js'
 import { toRadian } from '../../utils/math.js'
+import { darken } from '../../utils/colors.js'
 
 // Config
 import CONFIG from './config.js'
@@ -374,23 +375,8 @@ class SceneManager extends EventEmitter {
         break
       case 'pick-color':
         if (this.activeSubject) {
-          console.log(this.activeSubject)
           this.activeSubject.mesh.material.color = new THREE.Color(el.dataset.color)
           this.activeSubject.mesh.material.needsUpdate = true
-          /* Resta el porcentaje indicado a un color (RR, GG o BB) hexadecimal para oscurecerlo */
-          const subtractLight = function(color, amount){
-            let cc = parseInt(color,16) - amount;
-            let c = (cc < 0) ? 0 : (cc);
-            c = (c.toString(16).length > 1 ) ? c.toString(16) : `0${c.toString(16)}`;
-            return c;
-          }
-
-          /* Oscurece un color hexadecimal de 6 caracteres #RRGGBB segun el porcentaje indicado */
-          const darken = (color, amount) =>{
-            color = (color.indexOf("#")>=0) ? color.substring(1,color.length) : color;
-            amount = parseInt((255*amount)/100);
-            return color = `#${subtractLight(color.substring(0,2), amount)}${subtractLight(color.substring(2,4), amount)}${subtractLight(color.substring(4,6), amount)}`;
-          }
 
           this.activeSubject.materials.highlight.color = new THREE.Color(darken(el.dataset.color, 15))
           this.activeSubject.materials.highlight.needsUpdate = true
@@ -668,6 +654,7 @@ class SceneManager extends EventEmitter {
     const { controls } = this.cameraCtrl
     this.canvas.classList.remove('is-dragging')
     this.canvas.classList.remove('is-pointing')
+    document.querySelector('.colors-ui').classList.remove('is-open')
 
     switch (mode) {
       default:
