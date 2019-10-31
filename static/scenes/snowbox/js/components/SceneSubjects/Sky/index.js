503ac6ba9959b4b351913cc15fb6c627a0eac196
Robpayot <robpayot@gmail.com>
2019-11-01 10:47:42 +1100

    skybox v2

--- static/scenes/snowbox/js/components/SceneSubjects/Sky/index.js
+++ static/scenes/snowbox/js/components/SceneSubjects/Sky/index.js
@@ -4,7 +4,7 @@
 import { toRadian } from '../../../utils/math.js'
 
 const textureLoader = new THREE.TextureLoader()
-const imagePrefix = './img/skybox/';
+const imagePrefix = './img/skybox_v02/';
 var directions  = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
 var imageSuffix = ".png";
 const materialArray = []
@@ -20,18 +20,35 @@ class Sky {
   constructor(scene, camera) {
     this.scene = scene
     this.camera = camera
+
+    this.onGui = this.onGui.bind(this)
+
+    this.gui = new dat.GUI()
+    this.guiController = {
+      cube_scale: 1,
+      cube_y_pos: -20,
+    }
+    this.gui.add(this.guiController, 'cube_scale', 0.0, 5.0).onChange(this.onGui)
+    this.gui.add(this.guiController, 'cube_y_pos', -100, 100).onChange(this.onGui)
+
+
     this.init()
   }
 
   init() {
 
-    var skyGeometry = new THREE.CubeGeometry( 50, 50, 50 );
+    var skyGeometry = new THREE.CubeGeometry( 62, 62, 62 );
 
     var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
-    var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
-    skyBox.position.y = -15
-    skyBox.rotation.y = Math.PI / 2
-    this.scene.add( skyBox );
+    this.skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
+    this.skyBox.position.y = this.guiController.cube_y_pos
+    this.skyBox.rotation.y = Math.PI / 2
+    this.scene.add( this.skyBox );
+  }
+
+  onGui() {
+    this.skyBox.position.y = this.guiController.cube_y_pos
+    this.skyBox.scale.set(this.guiController.cube_scale, this.guiController.cube_scale, this.guiController.cube_scale)
   }
 }
 
