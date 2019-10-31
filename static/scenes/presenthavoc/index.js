9203268d6eddf6f17c177c65d1dc61700389aa52
Le <le@upperquad.com>
2019-10-31 14:08:16 +1100

    Wip

---
+++ static/scenes/presenthavoc/index.js
@@ -0,0 +1,27 @@
+import '../../src/magic.js';
+import api from '../../src/scene/api.js';
+
+import Game from './js/game.js'
+
+api.preload.images(
+  'img/car-body.svg',
+  'img/car-wheel.svg',
+  'img/fence.svg',
+  'img/player-a.svg',
+  'img/player-b.svg',
+  'img/table-1.svg',
+  'img/table-2.svg',
+  'img/table-3.svg',
+  'img/tree.svg',
+);
+
+api.preload.sounds('village_load_sounds');
+
+api.config({
+  sound: ['music_start_village', 'village_start'],
+});
+
+api.ready(async () => {
+  // do nothing
+});
+
