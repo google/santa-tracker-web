a390f792015964fa856cb0edb413576bff346a91
Robpayot <robpayot@gmail.com>
2019-11-04 14:38:10 +1100

    skybox_v4

--- static/scenes/snowbox/js/components/SceneSubjects/Sky/config.js
+++ static/scenes/snowbox/js/components/SceneSubjects/Sky/config.js
@@ -1,6 +1,6 @@
 export default {
   NAME: 'skybox',
-  PREFIX: './img/skybox_v03/',
+  PREFIX: './img/skybox_v04/',
   DIRECTIONS: ['xpos', 'xneg', 'ypos', 'yneg', 'zpos', 'zneg'],
   SUFFIX: '.png'
 }
