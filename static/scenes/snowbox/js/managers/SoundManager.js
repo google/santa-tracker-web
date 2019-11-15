4d4b23c8d5663a23133478947c8ee5aa5bca24ad
Andreas Jeppsson <andreas@plan8.se>
2019-11-15 12:32:01 +0100

    collision mass fix

--- static/scenes/snowbox/js/managers/SoundManager.js
+++ static/scenes/snowbox/js/managers/SoundManager.js
@@ -7,9 +7,19 @@ class SoundManager {
   playCollisionSound(e) {
     const { force, type, mass, scale } = e.detail
 
+    let maxMassForShape = {
+      arch: 1280,
+      sphere:83.77,
+      tree:205,
+      cube: 514.78
+    }
+    
+    
     let now = performance.now();
     if (now - this.lastCollisionTime > 50) {
-      let pitch = Math.abs(1 - (mass / 80)) + 0.5 + Math.random() * 0.2;
+      let maxMass = maxMassForShape[type] || 100;
+      let massPitch = Math.min(2, Math.max(0.3, mass / maxMass));
+      let pitch = Math.abs(1 - massPitch) + 0.5 + Math.random() * 0.2;
       let volume = Math.max(0, Math.min(1, (force - 0.5 ) / 3 ));
       this.play("snowbox_collision", volume, pitch, mass > 40);
       this.lastCollisionTime = now;
