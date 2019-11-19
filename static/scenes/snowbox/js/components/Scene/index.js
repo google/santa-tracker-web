f560e2896058ae29b14a6e8ce720d2bb3b9fce51
Robpayot <robpayot@gmail.com>
2019-11-18 16:45:11 -0800

    FIx safari shapes

--- static/scenes/snowbox/js/components/Scene/index.js
+++ static/scenes/snowbox/js/components/Scene/index.js
@@ -118,6 +118,101 @@ class Scene extends EventEmitter {
     }
 
     this.events()
+
+    // Global vars to cache event state
+    var evCache = new Array();
+    var prevDiff = -1;
+
+     this.canvas.onpointerdown = pointerdown_handler;
+     this.canvas.onpointermove = pointermove_handler;
+
+     // Use same handler for pointer{up,cancel,out,leave} events since
+     // the semantics for these events - in this app - are the same.
+     this.canvas.onpointerup = pointerup_handler;
+     this.canvas.onpointercancel = pointerup_handler;
+     this.canvas.onpointerout = pointerup_handler;
+     this.canvas.onpointerleave = pointerup_handler;
+
+
+    function pointerdown_handler(ev) {
+     // The pointerdown event signals the start of a touch interaction.
+     // This event is cached to support 2-finger gestures
+     evCache.push(ev);
+     console.log("pointerDown", ev);
+    }
+
+    function pointerup_handler(ev) {
+      console.log(ev.type, ev);
+      // Remove this pointer from the cache and reset the target's
+      // background and border
+      remove_event(ev);
+      ev.target.style.background = "white";
+      ev.target.style.border = "1px solid black";
+
+      // If the number of pointers down is less than two then reset diff tracker
+      if (evCache.length < 2) {
+        prevDiff = -1;
+      }
+    }
+
+    function pointermove_handler(ev) {
+     // This function implements a 2-pointer horizontal pinch/zoom gesture.
+     //
+     // If the distance between the two pointers has increased (zoom in),
+     // the taget element's background is changed to "pink" and if the
+     // distance is decreasing (zoom out), the color is changed to "lightblue".
+     //
+     // This function sets the target element's border to "dashed" to visually
+     // indicate the pointer's target received a move event.
+     console.log("pointerMove", ev);
+     ev.target.style.border = "dashed";
+     alert('pointerMove in')
+
+     // Find this event in the cache and update its record with this event
+     for (var i = 0; i < evCache.length; i++) {
+       if (ev.pointerId == evCache[i].pointerId) {
+          evCache[i] = ev;
+       break;
+       }
+     }
+
+     // If two pointers are down, check for pinch gestures
+     if (evCache.length == 2) {
+       // Calculate the distance between the two pointers
+       var curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
+
+       if (prevDiff > 0) {
+         if (curDiff > prevDiff) {
+           // The distance between the two pointers has increased
+           console.log("Pinch moving OUT -> Zoom in", ev);
+           alert('zoom in')
+           ev.target.style.background = "pink";
+           CameraController.zoom('in', true)
+
+         }
+         if (curDiff < prevDiff) {
+            alert('zoom out')
+           // The distance between the two pointers has decreased
+           console.log("Pinch moving IN -> Zoom out", ev);
+           ev.target.style.background = "lightblue";
+           CameraController.zoom('out', true)
+         }
+       }
+
+       // Cache the distance for the next move event
+       prevDiff = curDiff;
+     }
+    }
+
+    function remove_event(ev) {
+     // Remove this event from the target's cache
+     for (var i = 0; i < evCache.length; i++) {
+       if (evCache[i].pointerId == ev.pointerId) {
+         evCache.splice(i, 1);
+         break;
+       }
+     }
+    }
   }
 
   preloadShapes() {
