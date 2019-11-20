7fbef77c7d31b88ae605dfaeb362f6272c0dc655
Robpayot <robpayot@gmail.com>
2019-11-20 12:45:57 -0800

    remove customEvent polyfill

--- static/scenes/snowbox/js/utils/createCustomEvent.js
+++ static/scenes/snowbox/js/utils/createCustomEvent.js
@@ -1,18 +1,3 @@
-//custom event ie11 polyfill
-(function () {
-
-  if ( typeof window.CustomEvent === "function" ) return false;
-
-  function CustomEvent ( event, params ) {
-    params = params || { bubbles: false, cancelable: false, detail: null };
-    var evt = document.createEvent( 'CustomEvent' );
-    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-    return evt;
-   }
-
-  window.CustomEvent = CustomEvent;
-})();
-
 export default function createCustomEvent(eventName, data = {}) {
   const event = new window.CustomEvent(eventName, {
     detail: data,
