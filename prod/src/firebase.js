9fe33d8bc72767e7d91729f622f2ea45a2118a5e
Robpayot <robpayot@gmail.com>
2019-11-20 12:40:33 -0800

    revert prod/src/firebase.js

--- prod/src/firebase.js
+++ prod/src/firebase.js
@@ -5,19 +5,30 @@
 import firebase from 'firebase/app';
 import 'firebase/remote-config';
 import defaults from './remote-config-defaults.js';
+import isAndroidTWA from './android-twa.js';
 
-export var firebaseConfig = {
-  apiKey: 'AIzaSyDdDYk51KwSIJ-vyBI01V3h87DaGYJ9DxQ',
-  authDomain: 'santaweb-demo.firebaseapp.com',
-  databaseURL: 'https://santaweb-demo.firebaseio.com',
-  projectId: 'santaweb-demo',
-  storageBucket: 'santaweb-demo.appspot.com',
-  messagingSenderId: '405945309789',
-  appId: '1:405945309789:web:beebb797c697146e9c0bb4',
-  measurementId: 'G-4CXCGE4LE6',
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBrNcGcna0TMn2uLRxhMBwxVwXUBjlZqzU',
+  authDomain: 'santa-api.firebaseapp.com',
+  databaseURL: 'https://santa-api.firebaseio.com',
+  projectId: 'santa-api',
+  storageBucket: 'santa-api.appspot.com',
+  messagingSenderId: '593146395815',
+  appId: '1:593146395815:web:d766962076fbbd13492f82',
+  measurementId: 'G-EWRYGZS6D3',
 };
 
 export function initialize() {
+  if (isAndroidTWA()) {
+    // Swap for TWA (dev and prod)
+    firebaseConfig.appId = '1:593146395815:web:aefb4c5b5e01137f492f82';
+    firebaseConfig.measurementId = 'G-0X2VE68GZD';
+  } else if (window.location.hostname !== 'santatracker.google.com') {
+    // Swap for dev
+    firebaseConfig.appId = '1:593146395815:web:54c339298196fd10492f82';
+    firebaseConfig.measurementId = 'G-GPEHME4LVG';
+  }
+
   firebase.initializeApp(firebaseConfig);
 
   // Fetch RC, with a fetch timeout of 30s and a key expiry of ~1 minute. This is reset later via
@@ -31,9 +42,9 @@ export function initialize() {
   remoteConfig.defaultConfig = defaults;
   window.firebase = firebase;  // side-effect
 
-  return remoteConfig.fetchAndActivate().catch(function(err) {
+  return remoteConfig.fetchAndActivate().catch((err) => {
     console.warn('could not fetch remoteConfig, using defaults', err);
-  }).then(function() {
+  }).then(() => {
     return remoteConfig;
   });
 }
