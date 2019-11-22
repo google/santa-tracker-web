e94d7a60fb66c3b28836aee1d7b202d625c929c8
Sam Thorogood <sam.thorogood@gmail.com>
2019-11-22 14:54:02 +1100

    better polyfills

--- static/support.js
+++ static/support.js
@@ -2,19 +2,21 @@
  * @fileoverview Polyfills required for legacy browsers.
  */
 
+// nb. webcomponents-loader polyfills:
+//   * Array.from
+//   * Object.assign
+//   * Promise
+//   * Symbol
+
 import 'core-js/modules/es.array.includes';
 import 'core-js/modules/es.string.starts-with';
 import 'core-js/modules/es.string.ends-with';
 import 'core-js/modules/es.string.includes';
 import 'core-js/modules/web.url-search-params';
+import 'whatwg-fetch';
 import 'regenerator-runtime/runtime';
 
 import './src/polyfill/classlist--toggle.js';
 import './src/polyfill/element--closest.js';
 import './src/polyfill/node.js';
 
-// nb. webcomponents-loader polyfills:
-//   * Array.from
-//   * Object.assign
-//   * Promise
-//   * Symbol
