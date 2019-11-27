cf7ab2689b7f4e8bc01f2ac6e63ab975b38402fe
Sam Thorogood <sam.thorogood@gmail.com>
2019-11-28 09:34:15 +1100

    fix => to real function for attribute polyfill

--- static/src/polyfill/attribute.js
+++ static/src/polyfill/attribute.js
@@ -1,6 +1,6 @@
 
 if (!Element.prototype.toggleAttribute) {
-  Element.prototype.toggleAttribute = (name, force) => {
+  Element.prototype.toggleAttribute = function(name, force) {
     force = Boolean(force);
 
     if (this.hasAttribute(name)) {
