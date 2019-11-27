4b29f4fa455a5373da24a17790580ae9c1d386c8
Sam Thorogood <samthor@users.noreply.github.com>
2019-11-27 13:31:50 +1100

    optimization fixes, remove wear (#173)

--- static/scenes/familyguide/index.js
+++ static/scenes/familyguide/index.js
@@ -3,6 +3,6 @@ import Accordion from './js/accordion.js';
 import { rectify } from '../../src/scene/route.js';
 
 api.ready(async () => {
+  rectify(document.body);  // nb. Accordion does this to itself
   new Accordion(document.querySelector('[data-familyguide-accordion]'));
-  rectify(document.body);
 });
--- static/scenes/familyguide/js/strings.js
+++ static/scenes/familyguide/js/strings.js
@@ -9,22 +9,20 @@ const models = [
     },
     cards: [
       {
+        id: 'codelab',
         title: _msg`scene_codelab`,
         body: _msg`familyguide_card_01_body`,
-        image: '../../img/scenes/codelab_1x.png',
-        href: 'codelab.html',
       },
+      // TODO(samthor): Restore once snowflake game is back.
+      // {
+      //   id: 'snowflake',
+      //   title: _msg`scene_snowflake`,
+      //   body: _msg`familyguide_card_02_body`,
+      // },
       {
-        title: _msg`scene_snowflake`,
-        body: _msg`familyguide_card_02_body`,
-        image: '../../img/scenes/snowflake_1x.png',
-        href: 'snowflake.html',
-      },
-      {
+        id: 'codeboogie',
         title: _msg`scene_codeboogie`,
         body: _msg`familyguide_card_03_body`,
-        image: '../../img/scenes/codeboogie_1x.png',
-        href: 'codeboogie.html',
       },
     ]
   },
@@ -36,32 +34,29 @@ const models = [
     },
     cards: [
       {
+        id: 'traditions',
         title: _msg`scene_traditions`,
         body: _msg`familyguide_card_04_body`,
-        image: '../../img/scenes/traditions_1x.png',
-        href: 'traditions.html',
       },
       {
+        id: 'translations',
         title: _msg`scene_translations`,
         body: _msg`familyguide_card_05_body`,
-        image: '../../img/scenes/translations_1x.png',
-        href: 'translations.html',
       },
       {
+        id: 'mercator',
         title: _msg`scene_mercator`,
         body: _msg`familyguide_card_06_body`,
-        image: '../../img/scenes/mercator_1x.png',
-        href: 'mercator.html',
       },
       {
+        id: 'traditionsquiz',
         title: _msg`scene_traditionsquiz`,
         body: _msg`familyguide_card_07_body`,
-        image: '../../img/scenes/traditionsquiz_1x.png',
         href: 'https://earth.google.com/web/@43.129579,-112.010821,1838a,2269637d,35y,0.00000182h,0.4t,0r/data=CisSKRIgZDRkYTI4YTFkMmYwMTFlODk2YTFjZDc4YmJiZmMxYTkiBXNwbC0w',
-        external: true,
       },
       /* TODO: Holding GOOGLE EARTH HOLIDAY FOODS card */
       // {
+      //   id: 'holdiayfoods',
       //   title: _msg`scene_holidayfoods`,
       //   body: _msg`familyguide_card_08_body`,
       //   image: '',
@@ -77,28 +72,24 @@ const models = [
     },
     cards: [
       {
+        id: 'santascanvas',
         title: _msg`scene_canvas`,
         body: _msg`familyguide_card_09_body`,
-        image: '../../img/scenes/santascanvas_1x.png',
-        href: 'santascanvas.html',
       },
       {
+        id: 'snowbox',
         title: _msg`scene_snowbox`,
         body: _msg`familyguide_card_10_body`,
-        image: '../../img/scenes/snowbox_1x.png',
-        href: 'snowbox.html',
       },
       {
+        id: 'elfmaker',
         title: _msg`scene_elfmaker`,
         body: _msg`familyguide_card_11_body`,
-        image: '../../img/scenes/elfmaker_1x.png',
-        href: 'elfmaker.html',
       },
       {
+        id: 'santaselfie',
         title: _msg`scene_santaselfie`,
         body: _msg`familyguide_card_12_body`,
-        image: '../../img/scenes/santaselfie_1x.png',
-        href: 'santaselfie.html',
       },
     ]
   },
@@ -110,29 +101,25 @@ const models = [
     },
     cards: [
       {
+        id: 'museum',
         title: _msg`scene_videoscene_museum`,
         body: _msg`familyguide_card_13_body`,
-        image: '../../img/scenes/museum_1x.png',
-        href: 'museum.html',
       },
       {
+        id: 'carpool',
         title: _msg`scene_videoscene_carpool`,
         body: _msg`familyguide_card_14_body`,
-        image: '../../img/scenes/carpool_1x.png',
-        href: 'carpool.html',
       },
       {
+        id: 'liftoff',
         title: _msg`scene_videoscene_liftoff`,
         body: _msg`familyguide_card_15_body`,
-        image: '../../img/scenes/takeoff_1x.png',
-        href: 'liftoff.html',
       },
       /* TODO: Holding OLLIE UNDER THE SEA card */
       // {
+      //   id: 'storybook',
       //   title: _msg`scene_ollieundersea`,
       //   body: _msg`familyguide_card_16_body`,
-      //   image: '../../img/scenes/horatio_1x.png',
-      //   href: '#',
       // },
     ]
   },
