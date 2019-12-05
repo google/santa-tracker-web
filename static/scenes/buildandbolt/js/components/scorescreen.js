125bd33a4678f654c2c1cd94d29c2501cbfd848d
Robpayot <robpayot@gmail.com>
2019-12-05 15:50:25 -0800

    wip

---
+++ static/scenes/buildandbolt/js/components/scorescreen.js
@@ -0,0 +1,57 @@
+goog.provide('app.ScoreScreen')
+
+goog.require('app.shared.utils')
+goog.require('Constants')
+
+app.ScoreScreen = class ScoreScreen {
+  constructor(game) {
+    this.game = game
+    this.guiElem = this.game.context.querySelector('[data-gui]')
+    this.playerSelectionScreen = this.game.context.querySelector('[data-player-selection]')
+    this.playerSelectionOptions = this.game.context.querySelectorAll('[data-player-option]')
+    this.controlsScreen = this.game.context.querySelector('[data-player-controls]')
+    this.controlsButton = this.game.context.querySelector('[data-player-controls-skip]')
+
+    if (app.shared.utils.touchEnabled) {
+      this.playerOption = Constants.PLAYER_OPTIONS.SINGLE
+      this.controlsScreen.classList.remove('is-hidden')
+    } else {
+      this.playerSelectionScreen.classList.remove('is-hidden')
+      this.playerSelectionOptions.forEach((element) => {
+          element.addEventListener('click',
+              this.onPlayerOptionClick.bind(this, element))
+
+          element.addEventListener('mouseenter', this.onPlayerOptionOver.bind(this, element))
+      })
+    }
+
+    this.controlsButton.addEventListener('click',
+        this.onSkipControlsClick.bind(this))
+    this.controlsButton.addEventListener('mouseenter',
+        this.onSkipControlsOver.bind(this))
+
+    // Debug mode
+    // this.onPlayerOptionClick(this.playerSelectionOptions[1])
+    // this.onSkipControlsClick()
+    // end debug mode
+  }
+
+  onPlayerOptionClick(element) {
+    this.playerSelectionScreen.classList.add('is-hidden')
+    this.controlsScreen.classList.remove('is-hidden')
+    this.playerOption = element.getAttribute('data-player-option')
+    window.santaApp.fire('sound-trigger', 'generic_button_click');
+  }
+  onPlayerOptionOver(element) {
+    window.santaApp.fire('sound-trigger', 'generic_button_over');
+  }
+  onSkipControlsClick() {
+    this.game.init(this.playerOption)
+    this.controlsScreen.classList.add('is-hidden')
+    this.guiElem.classList.add('game-started')
+    window.santaApp.fire('sound-trigger', 'generic_button_click');
+  }
+  onSkipControlsOver(element) {
+    window.santaApp.fire('sound-trigger', 'generic_button_over');
+  }
+}
