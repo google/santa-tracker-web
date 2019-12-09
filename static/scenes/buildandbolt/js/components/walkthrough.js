72f06afe0441b26025c4b41a8274b56768cd9b55
Robpayot <robpayot@gmail.com>
2019-12-09 11:57:19 -0800

    css walktrhough

---
+++ static/scenes/buildandbolt/js/components/walkthrough.js
@@ -0,0 +1,21 @@
+goog.provide('app.Walkthrough')
+
+class	Walkthrough {
+ 	init(elem, game) {
+
+ 	}
+
+ 	show() {
+
+ 	}
+
+ 	hide() {
+
+ 	}
+
+ 	update() {
+
+ 	}
+}
+
+app.Walkthrough = new Walkthrough()
