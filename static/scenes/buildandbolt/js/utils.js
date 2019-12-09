9f867c041897f88dbf2fb1dfb036c91a5060a746
Le <le@upperquad.com>
2019-12-09 11:19:41 -0800

    Fix getDistance bug, switch player colors

--- static/scenes/buildandbolt/js/utils.js
+++ static/scenes/buildandbolt/js/utils.js
@@ -301,5 +301,5 @@ Utils.getAngle =  function(pos1, pos2) {
  * Get distance between two positions
  */
 Utils.getDistance =  function(pos1, pos2) {
-  return Math.sqrt(Math.pow(pos1.y - pos2.y, 2), Math.pow(pos1.x - pos2.x, 2))
+  return Math.sqrt(Math.pow(pos1.y - pos2.y, 2) + Math.pow(pos1.x - pos2.x, 2))
 }
