50332b09c820416a53c75e620fff2a130798ef5f
Le <le@upperquad.com>
2019-12-10 08:52:38 -0800

    Resolve gui comments

--- static/scenes/buildandbolt/js/components/gui.js
+++ static/scenes/buildandbolt/js/components/gui.js
@@ -1,35 +1,36 @@
-goog.provide('app.Gui')
+goog.provide('app.Gui');
 
-goog.require('app.shared.utils')
-goog.require('Constants')
+goog.require('app.shared.utils');
+goog.require('Constants');
 
 app.Gui = class Gui {
   constructor(game) {
-    this.game = game
-    this.guiElem = this.game.context.querySelector('[data-gui]')
-    this.playerSelectionScreen = this.game.context.querySelector('[data-player-selection]')
-    this.playerSelectionOptions = this.game.context.querySelectorAll('[data-player-option]')
-    this.controlsScreen = this.game.context.querySelector('[data-player-controls]')
-    this.controlsButton = this.game.context.querySelector('[data-player-controls-skip]')
+    this.game = game;
+    this.guiElem = this.game.context.querySelector('[data-gui]');
+    this.playerSelectionScreen = this.game.context.querySelector('[data-player-selection]');
+    this.playerSelectionOptions = this.game.context.querySelectorAll('[data-player-option]');
+    this.controlsScreen = this.game.context.querySelector('[data-player-controls]');
+    this.controlsButton = this.game.context.querySelector('[data-player-controls-skip]');
 
     if (app.shared.utils.touchEnabled) {
-      this.playerOption = Constants.PLAYER_OPTIONS.SINGLE
-      this.startGame()
-      return
-    } else {
-      this.playerSelectionScreen.classList.remove('is-hidden')
-      this.playerSelectionOptions.forEach((element) => {
-          element.addEventListener('click',
-              this.onPlayerOptionClick.bind(this, element))
-
-          element.addEventListener('mouseenter', this.onPlayerOptionOver.bind(this, element))
-      })
+      this.playerOption = Constants.PLAYER_OPTIONS.SINGLE;
+      this.startGame();
+      return;
     }
 
+    this.playerSelectionScreen.classList.remove('is-hidden');
+    this.playerSelectionOptions.forEach((element) => {
+        element.addEventListener('click',
+            this.onPlayerOptionClick.bind(this, element));
+
+        element.addEventListener('mouseenter', this.onPlayerOptionOver.bind(this, element));
+    });
+
+
     this.controlsButton.addEventListener('click',
-        this.onSkipControlsClick.bind(this))
+        this.onSkipControlsClick.bind(this));
     this.controlsButton.addEventListener('mouseenter',
-        this.onSkipControlsOver.bind(this))
+        this.onSkipControlsOver.bind(this));
 
     // Debug mode
     // this.onPlayerOptionClick(this.playerSelectionOptions[1])
@@ -38,29 +39,29 @@ app.Gui = class Gui {
   }
 
   onPlayerOptionClick(element) {
-    this.playerSelectionScreen.classList.add('is-hidden')
-    this.playerOption = element.getAttribute('data-player-option')
-    this.controlsScreen.classList.remove('is-hidden')
-    this.controlsScreen.setAttribute('data-player-controls', this.playerOption)
-    window.santaApp.fire('sound-trigger', 'generic_button_click')
+    this.playerSelectionScreen.classList.add('is-hidden');
+    this.playerOption = element.getAttribute('data-player-option');
+    this.controlsScreen.classList.remove('is-hidden');
+    this.controlsScreen.setAttribute('data-player-controls', this.playerOption);
+    window.santaApp.fire('sound-trigger', 'generic_button_click');
   }
 
   onPlayerOptionOver(element) {
-    window.santaApp.fire('sound-trigger', 'generic_button_over')
+    window.santaApp.fire('sound-trigger', 'generic_button_over');
   }
 
   onSkipControlsClick() {
-    this.controlsScreen.classList.add('is-hidden')
-    this.startGame()
-    window.santaApp.fire('sound-trigger', 'generic_button_click')
+    this.controlsScreen.classList.add('is-hidden');
+    this.startGame();
+    window.santaApp.fire('sound-trigger', 'generic_button_click');
   }
 
   onSkipControlsOver(element) {
-    window.santaApp.fire('sound-trigger', 'generic_button_over')
+    window.santaApp.fire('sound-trigger', 'generic_button_over');
   }
 
   startGame() {
-    this.game.init(this.playerOption)
-    this.guiElem.classList.add('game-started')
+    this.game.init(this.playerOption);
+    this.guiElem.classList.add('game-started');
   }
 }
