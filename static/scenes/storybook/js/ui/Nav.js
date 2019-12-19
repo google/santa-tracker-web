9f6eabee66bad7112fe2a9e1ad5aaeaa3ccd35a6
Yanlin-Ma <ym1399@nyu.edu>
2019-12-18 19:43:14 -0800

    Add swip on mobile

--- static/scenes/storybook/js/ui/Nav.js
+++ static/scenes/storybook/js/ui/Nav.js
@@ -4,8 +4,13 @@ import Transition from '../components/Transition.js';
 
 class Nav {
   constructor() {
+    this.xDown = null;
+    this.yDown = null;
+
     this.prev = this.prev.bind(this);
     this.next = this.next.bind(this);
+    this.handleTouchStart = this.handleTouchStart.bind(this);
+    this.handleTouchMove = this.handleTouchMove.bind(this);
   }
 
   init(el, index, length) {
@@ -24,7 +29,30 @@ class Nav {
     this.nextBtn.addEventListener('click', this.next);
     this.prevBtn.addEventListener('touchstart', this.prev);
     this.nextBtn.addEventListener('touchstart', this.next);
+    document.addEventListener('touchstart', this.handleTouchStart, false);
+    document.addEventListener('touchmove', this.handleTouchMove, false);
   }
+
+  handleTouchStart(evt) {
+    this.xDown = evt.touches[0].clientX;
+    this.yDown = evt.touches[0].clientY;
+  };
+
+  handleTouchMove(evt) {
+    if ( !this.xDown || !this.yDown ) { return; }
+
+    const xUp = evt.touches[0].clientX;
+    const yUp = evt.touches[0].clientY;
+    const xDiff = this.xDown - xUp;
+    const yDiff = this.yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+      xDiff > 0 ? this.next() : this.prev();
+    }
+
+    this.xDown = null;
+    this.yDown = null;
+  };
 
   prev() {
     this.activeIndex = this.activeIndex > 0 ? this.activeIndex - 1 : this.pages - 1;
