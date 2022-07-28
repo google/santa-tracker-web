94955d1e73e62061064e9f40e7cef081bfd3a276
Vincent Scheib <scheib@users.noreply.github.com>
2022-07-28 12:48:28 -0700

    Elf Ski: Add keyboard and gamepad support. (#98)
    Refactors physics.js to operate on an angle representation instead of vector.
    This makes it easier to receive input as a target angle, which can come
    from pointer events or keyboard.
    
    Gamepad input is already converted to synthetic arrow keydown events
    by keys.js, so now gamepad input also works.

--- static/scenes/elfski/js/physics.js
+++ static/scenes/elfski/js/physics.js
@@ -20,8 +20,7 @@ const slerpRatio = 4;
 const accelerationBar = 0.5;
 const maximumSpeed = 0.66;
 const stopBar = 0.75;  // angle at which we are trying to stop
-const initialVec = {x: 1, y: 0};
-
+const initialAngle = Math.PI/2;
 
 export class Character {
   constructor() {
@@ -29,7 +28,7 @@ export class Character {
     this._speed = 0;
     
     /** @private {vec.Vector} */
-    this._vec = initialVec;
+    this._angle = initialAngle;
 
     /** @private {number} */
     this._line = 0;
@@ -60,22 +59,21 @@ export class Character {
    * @return {number} angle in rads
    */
   get angle() {
-    return Math.atan2(this._vec.x, this._vec.y);
+    return this._angle;
   }
 
   /**
    * @return {vec.Vector} angle vector
    */
   get angleVec() {
-    return this._vec;
+    return {x: Math.sin(this._angle), y: Math.cos(this._angle)}
   }
 
   /**
    * @param {number} v angle in rads
    */
   set angle(v) {
-    this._vec.x = Math.cos(v);
-    this._vec.y = Math.sin(v);
+    this._angle = v;
   }
 
   /**
@@ -83,7 +81,7 @@ export class Character {
    */
   crash() {
     this._speed = 0;
-    this._vec = {x: (this._vec.x > 0) ? +1 : -1, y: 0};
+    this._angle = math.sign(this._angle) * initialAngle;
   }
 
   /**
@@ -91,32 +89,26 @@ export class Character {
    */
   reset() {
     this._speed = 0;
-    this._vec = initialVec;
+    this._angle = initialAngle;
     this._line = 0;
   }
 
   /**
    * @param {number} delta as fraction of second
-   * @param {?vec.Vector=} target relative to character position
+   * @param {?number} targetAngle in radians. 0 is straight down == positive y axis.
    * @return {{x: number, y: number}} amount to move by
    * @export
    */
-  tick(delta, target=null) {
-    if (target) {
-      target = vec.unitVec(target);  // deal with bad client data
+  tick(delta, targetAngle) {
+    if (Number.isFinite(targetAngle)) {
+      // Clamp target angle at most being horizontal left or right 90 degrees.
+      targetAngle = Math.min(Math.max(-Math.PI/2, targetAngle), Math.PI/2);
 
-      // player is drifting "up", so retain vector
-      if (target.y < 0 && this._vec.y < stopBar) {
-        target = {
-          x: Math.sign(this._vec.x),
-          y: 0,
-        };
-      }
-
+      // Adjust angle towards target based on speed.
       const by = slerpRatio * delta * (maximumSpeed * 2 - this._speed);
-      this._vec = vec.slerp(this._vec, target, by);
+      this._angle = this._angle + by * (targetAngle - this._angle);
 
-      this._line = vec.angleBetween(this._vec, target);  // difference between player/goal
+      this._line = Math.abs(targetAngle - this._angle);  // difference between player/goal
     } else {
       this._line = 0;  // character not changing direction
     }
@@ -136,7 +128,7 @@ export class Character {
 
     return {
       x: this._speed * Math.sin(angle) * delta,
-      y: this._speed * this._vec.y * delta,
+      y: this._speed * Math.cos(angle) * delta,
     };
   }
 }
